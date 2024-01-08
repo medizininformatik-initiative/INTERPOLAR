a29f078f98f11f1d62725f55a45894e1604b33e3
Alexander Strübing <alexander.struebing@gmail.com>
2024-01-08 22:20:28 +0100

    Add automatic sourcing of subscripts in Main

--- R-ETL/interpolar.R.ETL/R/00_Main.R
+++ R-ETL/interpolar.R.ETL/R/00_Main.R
@@ -16,16 +16,6 @@ PROJECT_NAME <<- 'interpolar'
 
 interpolar.R.utils::create_dirs(PROJECT_NAME)
 
-source('R/01_Table_Description.R')
-
-##
-# print disclaimer if Verbose Level allows it
-###
-# if (1 <= VERBOSE) {
-#   browser()
-#   cat(interpolar.R.utils::polar_disclaimer())
-# }
-
 ###
 # STOP is set by a Script to TRUE, if this script 'wants' to stop the Scripts Loop
 ###
@@ -36,20 +26,15 @@ STOP <- FALSE
 ###
 interpolar.R.utils::start_logging('retrieval-total')
 
-interpolar.R.utils::run_in(toupper('get_encounters'), {
-PERIOD_START <- "2019-01-02"
-PERIOD_END <- "2019-06-02"
-browser()
-interpolar.R.utils::get_encounters()
-})
-
-#build FHIR-Server-Requests
-request <- fhircrackr::fhir_url(url = FHIR_SERVER_ENDPOINT, resource = "Encounter")
-#download and crack bundles
-bundles <- fhircrackr::fhir_search(request, max_bundles = 5)
-table_enc <- fhircrackr::fhir_crack(bundles = bundles, design = TABLE_DESCRIPTION$Encounter, verbose = 10)
-
 ###
-# Save all console logs
+# Source all R files with a number prefix > 00 in R/ directory (this file here starts
+# with 00 and will not be sourced again.
 ###
+for (file in sort(dir(path = 'R/', pattern = '^[0-9][1-9]_.+\\.R$', full.names = TRUE))) {
+  source(file)
+}
+
+# ###
+# # Save all console logs
+# ###
 interpolar.R.utils::end_logging()
