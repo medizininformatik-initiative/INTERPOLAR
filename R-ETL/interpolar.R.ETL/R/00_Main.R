819b79b8ef10fc06f5fa8cb315184d22868b2e06
Alexander Strübing <alexander.struebing@gmail.com>
2024-01-11 21:26:01 +0100

    Make 00_Main package conform

--- R-ETL/interpolar.R.ETL/R/00_Main.R
+++ R-ETL/interpolar.R.ETL/R/00_Main.R
@@ -1,40 +1,44 @@
-###
-# Read the configuration toml file
-###
+#'
+#' Starts the retrieval for this project. This is the main start function start the ETL job
+#' from FHIR to Database
+#'
+#' @export
+retrieve <- function() {
 
-interpolar.R.utils::initConstants('../interpolar_R_ETL_config.toml')
+  ###
+  # Read the configuration toml file
+  ###
+  interpolar.R.utils::initConstants('../interpolar_R_ETL_config.toml')
 
-###
-# Create globally used polar_clock
-###
-POLAR_CLOCK <- interpolar.R.utils::createClock()
+  ###
+  # Create globally used polar_clock
+  ###
+  POLAR_CLOCK <<- interpolar.R.utils::createClock()
 
-###
-# Set the project name to 'interpolar'
-PROJECT_NAME <<- 'interpolar'
-###
+  ###
+  # Set the project name to 'interpolar'
+  PROJECT_NAME <<- 'interpolar'
+  ###
 
-interpolar.R.utils::create_dirs(PROJECT_NAME)
+  interpolar.R.utils::create_dirs(PROJECT_NAME)
 
-###
-# STOP is set by a Script to TRUE, if this script 'wants' to stop the Scripts Loop
-###
-STOP <- FALSE
+  ###
+  # STOP is set by a Script to TRUE, if this script 'wants' to stop the Scripts Loop
+  ###
+  STOP <- FALSE
 
-###
-# log all console outputs and save them at the end
-###
-interpolar.R.utils::start_logging('retrieval-total')
+  ###
+  # log all console outputs and save them at the end
+  ###
+  interpolar.R.utils::start_logging('retrieval-total')
 
-###
-# Source all R files with a number prefix > 00 in R/ directory (this file here starts
-# with 00 and will not be sourced again.
-###
-for (file in sort(dir(path = 'R/', pattern = '^[0-9][1-9]_.+\\.R$', full.names = TRUE))) {
-  source(file)
+  interpolar.R.utils::run_out('Run Retrieve', {
+    patientIDs <- getInterpolarPatientIDs()
+  })
+
+  ###
+  # Save all console logs
+  ###
+  interpolar.R.utils::end_logging()
+
 }
-
-# ###
-# # Save all console logs
-# ###
-interpolar.R.utils::end_logging()
