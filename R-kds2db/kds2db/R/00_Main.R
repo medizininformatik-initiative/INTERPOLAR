64daf4bbdd85f81423187bc7f87ed53a01232a41
Alexander Strübing <alexander.struebing@gmail.com>
2024-02-01 15:19:14 +0100

    Rename R.ETL -> kds2db and R.utils -> mrputils

---
+++ R-kds2db/kds2db/R/00_Main.R
@@ -0,0 +1,60 @@
+#'
+#' Starts the retrieval for this project. This is the main start function start the ETL job
+#' from FHIR to Database
+#'
+#' @export
+retrieve <- function() {
+
+  ###
+  # Read the configuration toml file
+  ###
+  mrputils::initConstants('../kds2db_config.toml')
+
+  ###
+  # Create globally used polar_clock
+  ###
+  POLAR_CLOCK <<- mrputils::createClock()
+
+  ###
+  # Set the project name to 'kds2db'
+  PROJECT_NAME <<- 'kds2db'
+  ###
+
+  mrputils::create_dirs(PROJECT_NAME)
+
+  ###
+  # STOP is set by a Script to TRUE, if this script 'wants' to stop the Scripts Loop
+  ###
+  STOP <- FALSE
+
+  ###
+  # log all console outputs and save them at the end
+  ###
+  mrputils::start_logging('retrieval-total')
+
+  mrputils::run_out('Run Retrieve', {
+
+    mrputils::run_in('Extract Patient IDs', {
+      patientIDsPerWard <- getPatientIDsPerWard(ifelse(exists('PATH_TO_PID_LIST_FILE'), PATH_TO_PID_LIST_FILE, NA))
+    })
+
+    mrputils::run_in('Load Table Description', {
+      table_descriptions <- getTableDescriptions()
+    })
+
+    mrputils::run_in('Download and crack resources by Patient IDs per ward', {
+      resource_table_list <<- loadResourcesByPatientIDFromFHIRServer(patientIDsPerWard, table_descriptions)
+    })
+
+    mrputils::run_in('Write resource tables to database', {
+      writeTablesToDatabase(resource_table_list)
+    })
+
+  })
+
+  ###
+  # Save all console logs
+  ###
+  mrputils::end_logging()
+
+}
