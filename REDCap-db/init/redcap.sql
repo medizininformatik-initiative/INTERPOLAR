a9848a9a906f2e7f95c73b72f781063b1715bf56
Sebastian Stäubert <sebastian.staeubert@gmail.com>
2024-02-14 16:08:46 +0100

    REDCap-app initial Version (not working); update .gitignore; refs #84

---
+++ REDCap-db/init/redcap.sql
@@ -0,0 +1,7 @@
+-- Example for creating the database (in either MariaDB or MySQL)
+CREATE DATABASE IF NOT EXISTS `redcap`;
+
+-- (MARIADB ONLY) Example for creating the MariaDB user (replace the user and password with your own values)
+-- CREATE USER 'redcap_user'@'%' IDENTIFIED BY 'password_for_redcap_user';
+GRANT SELECT, INSERT, UPDATE, DELETE ON `redcap`.* TO 'redcap_user'@'%';
+
