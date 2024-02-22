e2508cd2a7c28b9563ff30e3e57fe5baf31b441f
fschmidt <florian.schmidt@imise.uni-leipzig.de>
2024-02-22 12:36:58 +0100

    U Print error in red color in console output

--- R-etlutils/etlutils/R/lib_clock.R
+++ R-etlutils/etlutils/R/lib_clock.R
@@ -263,7 +263,7 @@ Clock = setRefClass(
       last_row <- max(which(.history$state == 'RUNNING'))
       # add end time, error and state to this row
       .history[last_row, 'end'] <<- now_
-      .history[last_row, 'error'] <<- if (inherits(err, 'try-error')) err else ''
+      .history[last_row, 'error'] <<- if (inherits(err, 'try-error')) cat_red(err) else ''
       .history[last_row, 'state'] <<- if (inherits(err, 'try-error')) 'ERROR' else 'OK'
       # if 0 < verbose print some messages
       if (0 < verbose) {
