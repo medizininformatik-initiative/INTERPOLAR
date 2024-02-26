011c370d7aaad9bdd72a4b793111014d6f4855a2
Alexander Strübing <alexander.struebing@gmail.com>
2024-02-26 22:45:23 +0100

    K Create Table Statements for DB from Table Description by Script

---
+++ R-kds2db/kds2db/R/Preparation_Create_TableStatements.R
@@ -0,0 +1,49 @@
+# free memory
+rm(list = ls())
+
+getTableStatmentEndRows <- function() {
+  end_rows <- ''
+  end_rows <- paste0(end_rows, "input_datetime timestamp not null default CURRENT_TIMESTAMP,   -- Time at which the data record is inserted\n")
+  end_rows <- paste0(end_rows, "last_check_datetime timestamp DEFAULT NULL,   -- Time at which data record was last checked\n")
+  end_rows <- paste0(end_rows, "current_dataset_status varchar(50) DEFAULT 'input'   -- Processing status of the data record\n")
+  end_rows <- paste0(end_rows, ");\n\n")
+}
+
+createKDS2DBTableStatements <- function() {
+  table_description <- etlutils::readExcelFileAsTableList('./R-kds2db/kds2db/inst/extdata/Table_Description.xlsx')[['table_description']]
+  statements <- ''
+  last_table_name <- NA
+  for (row in 1:nrow(table_description)) {
+    table_name <- table_description$table[row]
+    if (!is.na(table_name)) {
+      if (is.na(last_table_name)) {
+        statements <- paste0(statements, getTableStatmentEndRows())
+      }
+      statements <- paste0(statements, "CREATE TABLE IF NOT EXISTS kds2db_in.", table_name, " (\n")
+      statements <- paste0(statements, "", table_name, "_id serial PRIMARY KEY not null, -- Primary key of the entity\n")
+    }
+    if (!all(is.na(table_description[row]))) {
+      count <- as.integer(table_description$count[row])
+      if (is.na(count)) count <- 1
+      single_length <- as.integer(table_description$single_length[row])
+      full_length <- single_length * count
+      fhir_expression <- table_description$fhir_expression[row]
+      statements <- paste0(statements, "", table_description$column_name[row], " varchar (", full_length, "),   -- ", fhir_expression, " (", single_length, " x ", count, " varchar)\n")
+    }
+
+  }
+  statements <- paste0(statements, getTableStatmentEndRows())
+}
+
+replacePlaceholders <- function() {
+  # Read the content of the file
+  #content <- readLines('./Postgres-amts-db', encoding = "UTF-8")
+  content <- readLines('./Postgres-amts_db/init/init-db_template.sql')
+  statements <- createKDS2DBTableStatements()
+  # Replace the placeholder with the statements
+  modified_content <- gsub("<%KDS2DB_CREATE_TABLE_STATEMENTS%>", statements, content)
+  # Write the modified content back to the file
+  writeLines(modified_content, './Postgres-amts_db/init/init-db_generated.sql', useBytes = TRUE)
+}
+
+replacePlaceholders()
