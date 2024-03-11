9b920a06801793ceb19963ad69fc3f9d3d65e4f7
fschmidt <florian.schmidt@imise.uni-leipzig.de>
2024-03-11 16:22:54 +0100

    Restructure scripts for text colorization

--- R-etlutils/etlutils/R/lib_colourise_text.R
+++ R-etlutils/etlutils/R/lib_colourise_text.R
@@ -1,9 +1,3 @@
-###
-#
-# Deprecated!!! Don't use these functions in future! Use function ansi() and cat_ansi() instead!
-#
-###
-
 #' Create escape ansi code for colored printing
 #'
 #' @param code An integer of length 1, the code of the color or an ansi code.
@@ -221,4 +215,82 @@ cat_warning <- function(msg, bg = NULL) {
 #' @export
 cat_info <- function(msg, bg = NULL) {
   cat_cyan(msg, bg = bg)
+}
+
+#' Styled String
+#'
+#' Format strings with ANSI escape codes for text styling.
+#'
+#' @param ... Character vectors to be styled.
+#' @param sep Separator between the styled strings.
+#' @param fg Foreground color (0-15).
+#' @param bg Background color (0-15).
+#' @param bold Use bold text.
+#' @param italic Use italic text.
+#' @param underline Underline text.
+#' @param slowblink Use slow blink effect.
+#' @param rapidblink Use rapid blink effect.
+#' @param invert Invert text colors.
+#' @param strike_out Strike out text.
+#'
+#' @return A styled string.
+#' @export
+styled_string <- function( #old name str.
+  ...,
+  sep        = '',
+  fg         = NULL,
+  bg         = NULL,
+  bold       = FALSE,
+  italic     = FALSE,
+  underline  = FALSE,
+  slowblink  = FALSE,
+  rapidblink = FALSE,
+  invert     = FALSE,
+  strike_out = FALSE
+) {
+  # collect all passed strings
+  strings <- list(...)
+
+  # if there are no strings, return
+  if (length(strings) < 1) {
+    return(strings)
+  }
+
+  # foreground and background have to be between 0 and 15
+  # if 7 < color, sub 8 and add 60 for brighter color code
+  if (!is.null(fg)) {
+    fg <- as.integer(fg) %% 16
+    if (7 < fg) fg <- fg + 52
+  }
+  if (!is.null(bg)) {
+    bg <- as.integer(bg) %% 16
+    if (7 < bg) bg <- bg + 52
+  }
+
+  # map arguments to its codes and paste them separated by a ;
+  codes <- paste0(
+    c(       1,      3,         4,         5,          6,      7,          9)[
+      c(bold, italic, underline, slowblink, rapidblink, invert, strike_out)
+    ],
+    collapse = ';'
+  )
+
+  # if all arguments were FALSE set codes to NULL
+  if (codes == '') codes <- NULL
+
+  # create color codes
+  colors <- paste(c(30 + fg, 40 + bg), collapse = ';')
+
+  # if no colors present, return NULL
+  if (colors == '') colors <- NULL
+
+  # collect codes and colors
+  codes <- paste0(c(codes, colors), collapse = ';')
+
+  # only paste the strings, if no codes given, otherwise paste the strings and surround them with ansi-codes
+  if (0 < length(codes)){
+    paste0('\033[', codes, 'm', paste(strings, collapse = sep), '\033[0m')
+  } else {
+    paste(strings, collapse = sep)
+  }
 }
