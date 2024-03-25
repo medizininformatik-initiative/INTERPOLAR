3c9890f52660dee43ab23f00e24d6a99d60bfce9
fschmidt <florian.schmidt@imise.uni-leipzig.de>
2024-03-25 11:34:24 +0100

    U Update tests for convertTimeFormat() function

--- R-etlutils/etlutils/tests/testthat/test-lib_helper_functions.R
+++ R-etlutils/etlutils/tests/testthat/test-lib_helper_functions.R
@@ -198,26 +198,17 @@ test_that("convertDateFormat converts date representations correctly", {
 #########################
 
 test_that("convertTimeFormat converts polar time representations to POSIXct", {
-  # Beispiel-Datentabelle mit gültigen Zeitformaten
-  dt <- data.table(column = c("12:30:45", "08:15:00", "23:59:59", "13:45:22.123456789"))
-
-  # Anwendung der Funktion auf die Spalte
-  convertTimeFormat(dt, "column")
-
-  # Überprüfen, ob die Zeitformate korrekt beibehalten wurden
-  expect_equal(dt$column, c("12:30:45", "08:15:00", "23:59:59", "13:45:22"))
-})
-
-# Testfall für leere Zeichenketten
-test_that("convertTimeFormat replaces empty strings with NA", {
-  # Beispiel-Datentabelle mit einer leeren Zeichenkette
-  dt <- data.table(column = c("", "08:15:00", "23:59:59", ""))
-
-  # Anwendung der Funktion auf die Spalte
-  convertTimeFormat(dt, "column")
-
-  # Überprüfen, ob leere Zeichenketten durch NA ersetzt wurden
-  expect_equal(dt$column, c(NA_character_, "08:15:00", "23:59:59", NA_character_))
+  # Create a test data.table
+  dt <- data.table(time_column = c("12:30:45", "08:15:00", "23:59:59", "13:45:22.123456789", NA, ""))
+  # Call the function
+  convertTimeFormat(dt, "time_column")
+  # Check specific time values
+  expect_equal(dt$time_column[1], as.POSIXct("1970-01-01 12:30:45", tz = "UTC"))
+  expect_equal(dt$time_column[2], as.POSIXct("1970-01-01 08:15:00", tz = "UTC"))
+  expect_equal(dt$time_column[3], as.POSIXct("1970-01-01 23:59:59", tz = "UTC"))
+  expect_equal(dt$time_column[4], as.POSIXct("1970-01-01 13:45:22", tz = "UTC"))
+  expect_true(is.na(dt$time_column[5]))  # Check NA value
+  expect_true(is.na(dt$time_column[6]))  # Check NA value
 })
 
 #########################
@@ -226,18 +217,18 @@ test_that("convertTimeFormat replaces empty strings with NA", {
 
 test_that("convertDateTimeFormat function converts datetime columns correctly", {
   # Test case 1: Valid time representation
-  dt <- data.table(column1 = c("2018", "1973-06", "1905-08-23",
+  dt <- data.table(datetime_column = c("2018", "1973-06", "1905-08-23",
                                "2015-02-07T13:28:17+01:00", "2015-02-07T13:28:17+03:00",
                                "2017-01-01T00:00:00.000Z", NA))
-  convertDateTimeFormat(dt, "column1")
+  convertDateTimeFormat(dt, "datetime_column")
   # Check if the datetime columns are converted properly
-  expect_equal(dt$column1[1], as.POSIXct("2018-01-01 00:00:00", tz = "Europe/Berlin"))
-  expect_equal(dt$column1[2], as.POSIXct("1973-06-01 00:00:00", tz = "Europe/Berlin"))
-  expect_equal(dt$column1[3], as.POSIXct("1905-08-23 00:00:00", tz = "Europe/Berlin"))
-  expect_equal(dt$column1[4], as.POSIXct("2015-02-07 13:28:17", tz = "Europe/Berlin"))
-  expect_equal(dt$column1[5], as.POSIXct("2015-02-07 11:28:17", tz = "Europe/Berlin"))
-  expect_equal(dt$column1[6], as.POSIXct("2017-01-01 01:00:00", tz = "Europe/Berlin"))
-  expect_equal(dt$column1[7], as.POSIXct(NA, tz = "Europe/Berlin"))
+  expect_equal(dt$datetime_column[1], as.POSIXct("2018-01-01 00:00:00", tz = "Europe/Berlin"))
+  expect_equal(dt$datetime_column[2], as.POSIXct("1973-06-01 00:00:00", tz = "Europe/Berlin"))
+  expect_equal(dt$datetime_column[3], as.POSIXct("1905-08-23 00:00:00", tz = "Europe/Berlin"))
+  expect_equal(dt$datetime_column[4], as.POSIXct("2015-02-07 13:28:17", tz = "Europe/Berlin"))
+  expect_equal(dt$datetime_column[5], as.POSIXct("2015-02-07 11:28:17", tz = "Europe/Berlin"))
+  expect_equal(dt$datetime_column[6], as.POSIXct("2017-01-01 01:00:00", tz = "Europe/Berlin"))
+  expect_equal(dt$datetime_column[7], as.POSIXct(NA, tz = "Europe/Berlin"))
 })
 
 #########################
