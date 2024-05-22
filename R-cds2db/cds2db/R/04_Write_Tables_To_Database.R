b742c15d19db9549695c3ebb6193cf322c6c5279
Alexander Strübing <alexander.struebing@gmail.com>
2024-05-22 07:18:02 +0200

    Change getting table names of RAW tables in writeTables() function

--- R-cds2db/cds2db/R/04_Write_Tables_To_Database.R
+++ R-cds2db/cds2db/R/04_Write_Tables_To_Database.R
@@ -3,33 +3,15 @@
 #' This function takes a list of data.table objects and writes them to a specified database.
 #' It establishes a database connection using predefined credentials and configuration parameters.
 #' Users have the option to clear existing table contents before writing new data by setting
-#' `clear_before_insert` to TRUE. The function can write all provided tables or only specific tables
-#' if specified by the `table_names` parameter.
+#' `clear_before_insert` to TRUE. The function writes all provided tables.
 #'
 #' @param tables A named list of data.table objects to be written to the database. Each element in
 #'               the list represents a dataset intended for a corresponding table in the database.
-#' @param table_names An optional vector of table names to specify which tables should be written
-#'                    to the database. If `NA` (default), all tables in the `tables` list are written.
 #' @param clear_before_insert A logical flag indicating whether to clear the table contents
 #'                            before inserting new data. Defaults to FALSE.
-#'
-#' @examples
-#' \dontrun{
-#'   # Example usage:
-#'   my_tables <- list(table1 = data.table(id = 1:3, data = c("A", "B", "C")),
-#'                     table2 = data.table(id = 4:6, data = c("D", "E", "F")))
-#'   writeTablesToDatabase(my_tables)
-#'
-#'   # Write only specific tables and clear contents before inserting
-#'   writeTablesToDatabase(my_tables, table_names = c("table1"), clear_before_insert = TRUE)
-#' }
-#'
-writeTablesToDatabase <- function(tables, table_names = NA, clear_before_insert = FALSE) {
+writeTablesToDatabase <- function(tables, clear_before_insert = FALSE) {
 
-  # write all tables (table_names == NA) or only tables with the given names
-  if (isSimpleNA(table_names)) {
-    table_names <- names(tables)
-  }
+  table_names <- names(tables)
 
   db_connection <- etlutils::dbConnect(
     user = DB_CDS2DB_USER,
