291fdf918749442feda8316d189359357202682f
Matthias Reusche <matthias.reusche@uni-leipzig.de>
2024-05-22 13:00:49 +0200

    correct the assignment of the sequence

--- Postgres-cds_hub/init/46_cre_table_typ_fe_in.sql
+++ Postgres-cds_hub/init/46_cre_table_typ_fe_in.sql
@@ -88,13 +88,14 @@ current_dataset_status varchar(50) DEFAULT 'input'   -- Bearbeitungstatus des Da
 );
 
 --SQL Role / Trigger in Schema db2frontend_in
+
 GRANT INSERT, DELETE, UPDATE, SELECT ON TABLE db2frontend_in.patient_fe TO db2frontend_user;
 GRANT INSERT, DELETE, UPDATE, SELECT ON TABLE db2frontend_in.patient_fe TO db_user;
 GRANT SELECT ON TABLE db2frontend_in.patient_fe TO db_log_user;
 GRANT TRIGGER ON db2frontend_in.patient_fe TO db2frontend_user;
-ALTER TABLE db2frontend_in.patient_fe ALTER COLUMN patient_fe_id SET DEFAULT (nextval('db2frontend_in.db2dataprocessor_in_seq'));
+ALTER TABLE db2frontend_in.patient_fe ALTER COLUMN patient_fe_id SET DEFAULT (nextval('db2frontend_in.db2frontend_in_seq'));
 GRANT USAGE ON SCHEMA db2frontend_in TO db2frontend_user;
-GRANT USAGE ON db2frontend_in.db2dataprocessor_in_seq TO db2frontend_user;
+GRANT USAGE ON db2frontend_in.db2frontend_in_seq TO db2frontend_user;
 
 CREATE OR REPLACE FUNCTION db2frontend_in.patient_fe_tr_ins_fkt()
 RETURNS TRIGGER AS $$
@@ -118,9 +119,9 @@ GRANT INSERT, DELETE, UPDATE, SELECT ON TABLE db2frontend_in.fall_fe TO db_user;
 GRANT SELECT ON TABLE db2frontend_in.fall_fe TO db2frontend_user;
 GRANT SELECT ON TABLE db2frontend_in.fall_fe TO db_log_user;
 GRANT TRIGGER ON db2frontend_in.fall_fe TO db2frontend_user;
-ALTER TABLE db2frontend_in.fall_fe ALTER COLUMN fall_fe_id SET DEFAULT (nextval('db2frontend_in.db2dataprocessor_in_seq'));
+ALTER TABLE db2frontend_in.fall_fe ALTER COLUMN fall_fe_id SET DEFAULT (nextval('db2frontend_in.db2frontend_in_seq'));
 GRANT USAGE ON SCHEMA db2frontend_in TO db2frontend_user;
-GRANT USAGE ON db2frontend_in.db2dataprocessor_in_seq TO db2frontend_user;
+GRANT USAGE ON db2frontend_in.db2frontend_in_seq TO db2frontend_user;
 
 CREATE OR REPLACE FUNCTION db2frontend_in.fall_fe_tr_ins_fkt()
 RETURNS TRIGGER AS $$
@@ -144,9 +145,9 @@ GRANT INSERT, DELETE, UPDATE, SELECT ON TABLE db2frontend_in.medikationsanalyse_
 GRANT SELECT ON TABLE db2frontend_in.medikationsanalyse_fe TO db2frontend_user;
 GRANT SELECT ON TABLE db2frontend_in.medikationsanalyse_fe TO db_log_user;
 GRANT TRIGGER ON db2frontend_in.medikationsanalyse_fe TO db2frontend_user;
-ALTER TABLE db2frontend_in.medikationsanalyse_fe ALTER COLUMN medikationsanalyse_fe_id SET DEFAULT (nextval('db2frontend_in.db2dataprocessor_in_seq'));
+ALTER TABLE db2frontend_in.medikationsanalyse_fe ALTER COLUMN medikationsanalyse_fe_id SET DEFAULT (nextval('db2frontend_in.db2frontend_in_seq'));
 GRANT USAGE ON SCHEMA db2frontend_in TO db2frontend_user;
-GRANT USAGE ON db2frontend_in.db2dataprocessor_in_seq TO db2frontend_user;
+GRANT USAGE ON db2frontend_in.db2frontend_in_seq TO db2frontend_user;
 
 CREATE OR REPLACE FUNCTION db2frontend_in.medikationsanalyse_fe_tr_ins_fkt()
 RETURNS TRIGGER AS $$
@@ -170,9 +171,9 @@ GRANT INSERT, DELETE, UPDATE, SELECT ON TABLE db2frontend_in.mrpdokumentation_va
 GRANT SELECT ON TABLE db2frontend_in.mrpdokumentation_validierung_fe TO db2frontend_user;
 GRANT SELECT ON TABLE db2frontend_in.mrpdokumentation_validierung_fe TO db_log_user;
 GRANT TRIGGER ON db2frontend_in.mrpdokumentation_validierung_fe TO db2frontend_user;
-ALTER TABLE db2frontend_in.mrpdokumentation_validierung_fe ALTER COLUMN mrpdokumentation_validierung_fe_id SET DEFAULT (nextval('db2frontend_in.db2dataprocessor_in_seq'));
+ALTER TABLE db2frontend_in.mrpdokumentation_validierung_fe ALTER COLUMN mrpdokumentation_validierung_fe_id SET DEFAULT (nextval('db2frontend_in.db2frontend_in_seq'));
 GRANT USAGE ON SCHEMA db2frontend_in TO db2frontend_user;
-GRANT USAGE ON db2frontend_in.db2dataprocessor_in_seq TO db2frontend_user;
+GRANT USAGE ON db2frontend_in.db2frontend_in_seq TO db2frontend_user;
 
 CREATE OR REPLACE FUNCTION db2frontend_in.mrpdokumentation_validierung_fe_tr_ins_fkt()
 RETURNS TRIGGER AS $$
@@ -197,26 +198,26 @@ comment on column db2frontend_in.patient_fe.pat_id is 'Patient-identifier (200 x
 comment on column db2frontend_in.patient_fe.pat_name is 'Patientenname (200 x 1 200 - varchar)';
 comment on column db2frontend_in.patient_fe.pat_vorname is 'Patientenvorname (200 x 1 200 - varchar)';
 comment on column db2frontend_in.patient_fe.pat_gebdat is 'Geburtsdatum (200 x 1 200 - varchar)';
-comment on column db2frontend_in.patient_fe.pat_aktuell_alter is '<div class="rich-text-field-label"><p>aktuelles Patientenalter (Jahre)</p></div> (200 x 1 200 - varchar)';
+comment on column db2frontend_in.patient_fe.pat_aktuell_alter is '<div class=rich-text-field-label><p>aktuelles Patientenalter (Jahre)</p></div> (200 x 1 200 - varchar)';
 comment on column db2frontend_in.patient_fe.pat_geschlecht is '0, nicht bekannt | 1, weiblich | 2, männlich | 3, diversGeschlecht (10 x 1 10 - numeric)';
 
 comment on column db2frontend_in.fall_fe.fall_id is 'Fall-ID (200 x 1 200 - varchar)';
-comment on column db2frontend_in.fall_fe.fall_studienphase is '1, Usual Care (UC) | 2, Interventional Care (IC) | 3, Pilotphase (P)<div class="rich-text-field-label"><p><span style="color: #e03e2d;">Studienphase (wird wenn produktiv versteckt sein)</span></p></div> (10 x 1 10 - numeric)';
+comment on column db2frontend_in.fall_fe.fall_studienphase is '1, Usual Care (UC) | 2, Interventional Care (IC) | 3, Pilotphase (P)<div class=rich-text-field-label><p><span style=color: #e03e2d;>Studienphase (wird wenn produktiv versteckt sein)</span></p></div> (10 x 1 10 - numeric)';
 comment on column db2frontend_in.fall_fe.fall_station is '1, Station 1 | 2, Station 2 | 3, Station 3Station (10 x 1 10 - numeric)';
 comment on column db2frontend_in.fall_fe.fall_aufn_dat is 'Aufnahmedatum (200 x 1 200 - varchar)';
-comment on column db2frontend_in.fall_fe.fall_aufn_diag is '<div class="rich-text-field-label"><p><span style="color: #e03e2d;">Diagnose(n) bei Aufnahme (wird nur zum lesen sein)</span></p></div> (1000 x 1 1000 - varchar)';
+comment on column db2frontend_in.fall_fe.fall_aufn_diag is '<div class=rich-text-field-label><p><span style=color: #e03e2d;>Diagnose(n) bei Aufnahme (wird nur zum lesen sein)</span></p></div> (1000 x 1 1000 - varchar)';
 comment on column db2frontend_in.fall_fe.fall_gewicht_kg_aktuell is 'aktuelles Gewicht (Kg) (500 x 1 500 - varchar)';
 comment on column db2frontend_in.fall_fe.fall_groesse_cm is 'Größe (cm) (230 x 1 230 - varchar)';
 comment on column db2frontend_in.fall_fe.fall_bmi is 'BMI (200 x 1 200 - varchar)';
 comment on column db2frontend_in.fall_fe.fall_nieren_insuf_chron is '1, ja | 0, nein | -1, nicht bekanntChronische Niereninsuffizienz (10 x 1 10 - numeric)';
-comment on column db2frontend_in.fall_fe.fall_nieren_insuf_ausmass is '1, Ausmaß unbekannt | 2, 45-59 ml/min/1,73 m2 | 3, 30-44 ml/min/1,73 m2 | 4, 15-29 ml/min/1,73 m2 | 5, < 15 ml/min/1,73 m2<div class="rich-text-field-label"><p>aktuelles Ausmaß</p></div> (10 x 1 10 - numeric)';
+comment on column db2frontend_in.fall_fe.fall_nieren_insuf_ausmass is '1, Ausmaß unbekannt | 2, 45-59 ml/min/1,73 m2 | 3, 30-44 ml/min/1,73 m2 | 4, 15-29 ml/min/1,73 m2 | 5, < 15 ml/min/1,73 m2<div class=rich-text-field-label><p>aktuelles Ausmaß</p></div> (10 x 1 10 - numeric)';
 comment on column db2frontend_in.fall_fe.fall_nieren_insuf_dialysev is '1, Hämodialyse | 2, Kont. Hämofiltration | 3, Peritonealdialyse | 4, keineDialyseverfahren (10 x 1 10 - numeric)';
 comment on column db2frontend_in.fall_fe.fall_leber_insuf is '1, ja | 0, nein | -1, nicht bekanntLeberinsuffizienz (10 x 1 10 - numeric)';
 comment on column db2frontend_in.fall_fe.fall_leber_insuf_ausmass is '1, Ausmaß unbekannt | 2, Leicht (Child-Pugh A) | 3, Mittel (Child-Pugh B) | 4, Schwer (Child-Pugh C)aktuelles Ausmaß (10 x 1 10 - numeric)';
-comment on column db2frontend_in.fall_fe.fall_schwanger_mo is '0, keine Schwangerschaft | 1, 1 | 2, 2 | 3, 3 | 4, 4 | 5, 5 | 6, 6 | 7, 7 | 8, 8 | 9, 9<div class="rich-text-field-label"><p><span style="color: #000000;">Schwangerschaftsmonat</span></p></div> (10 x 1 10 - numeric)';
+comment on column db2frontend_in.fall_fe.fall_schwanger_mo is '0, keine Schwangerschaft | 1, 1 | 2, 2 | 3, 3 | 4, 4 | 5, 5 | 6, 6 | 7, 7 | 8, 8 | 9, 9<div class=rich-text-field-label><p><span style=color: #000000;>Schwangerschaftsmonat</span></p></div> (10 x 1 10 - numeric)';
 comment on column db2frontend_in.fall_fe.fall_op_geplant is '1, ja | 0, nein | -1, nicht bekanntIst eine Operation geplant? (10 x 1 10 - numeric)';
 comment on column db2frontend_in.fall_fe.fall_op_dat is 'Operationsdatum (200 x 1 200 - varchar)';
-comment on column db2frontend_in.fall_fe.fall_status is '1, aktuell / neu | 2, nicht relevant | 3, verlegt<div class="rich-text-field-label"><p><span style="color: #e03e2d;"><span style="color: #000000;">Fallstatus:</span><br></span></p></div> (10 x 1 10 - numeric)';
+comment on column db2frontend_in.fall_fe.fall_status is '1, aktuell / neu | 2, nicht relevant | 3, verlegt<div class=rich-text-field-label><p><span style=color: #e03e2d;><span style=color: #000000;>Fallstatus:</span><br></span></p></div> (10 x 1 10 - numeric)';
 comment on column db2frontend_in.fall_fe.fall_ent_dat is 'Entlassdatum (200 x 1 200 - varchar)';
 
 comment on column db2frontend_in.medikationsanalyse_fe.meda_dat is 'Datum der Medikationsanalyse (200 x 1 200 - varchar)';
@@ -239,9 +240,9 @@ comment on column db2frontend_in.mrpdokumentation_validierung_fe.mrp_atc2 is '72
 comment on column db2frontend_in.mrpdokumentation_validierung_fe.mrp_atc3 is '7298, J05AF06 - Abacavir | 7299, H05AA04 - Abaloparatid | 7300, P03AX07 - Abametapir | 7301, L02BX01 - Abarelix | 7302, L04AA24 - Abatacept | 7303, B01AC13 - Abciximab | 7304, L01EF03 - Abemaciclib | 7305, L04AA22 - Abetimus | 7306, S01LA07 - Abicipar peg3. Medikament ATC / Name (10 x 1 10 - numeric)';
 comment on column db2frontend_in.mrpdokumentation_validierung_fe.mrp_med_prod is '1, Ja | 0, NeinMedizinprodukt betroffen? (10 x 1 10 - numeric)';
 comment on column db2frontend_in.mrpdokumentation_validierung_fe.mrp_med_prod_sonst is 'Sonstigespräparat (200 x 1 200 - varchar)';
-comment on column db2frontend_in.mrpdokumentation_validierung_fe.mrp_dokup_fehler is '<div class="rich-text-field-label"><p>Frage / <span style="font-weight: normal;">Fehlerbeschreibung </span></p> <p><span style="font-weight: normal;">[mrp_kurzbeschr]</span></p></div> (200 x 1 200 - varchar)';
-comment on column db2frontend_in.mrpdokumentation_validierung_fe.mrp_dokup_intervention is '<div class="rich-text-field-label"><p>Intervention / <span style="font-weight: normal;">Vorschlag zur Fehlervermeldung</span></p></div> (200 x 1 200 - varchar)';
-comment on column db2frontend_in.mrpdokumentation_validierung_fe.mrp_pi_grund_am is '1, (Klare) Indikation nicht (mehr) gegeben (MF) | 2, Verordnung/Dokumentation unvollständig/fehlerhaft (MF) | 3, Ungeeignetes/nicht am besten geeignetes Arzneimittel für die Indikation (MF) | 4, Ungeeignetes/nicht am besten geeignetes Arzneimittel bezüglich Kosten (MF) | 5, Ungeeignetes/nicht am besten geeignetes Arzneimittelform für die Indikation (MF) | 6, Übertragungsfehler (MF) | 7, Substitution aut idem/aut simile (MF) | 11, (Klare) Indikation, aber kein Medikament angeordnet (MF) | 12, Stellfehler (MF) | 13, Arzneimittelallergie oder anamnestische Faktoren nicht berücksichtigt (MF) | 14, Doppelverordnung (MF)<div class="rich-text-field-label"><p style="text-align: left;">AM: Arzneimittel</p></div> (10 x 1 10 - numeric)';
+comment on column db2frontend_in.mrpdokumentation_validierung_fe.mrp_dokup_fehler is '<div class=rich-text-field-label><p>Frage / <span style=font-weight: normal;>Fehlerbeschreibung </span></p> <p><span style=font-weight: normal;>[mrp_kurzbeschr]</span></p></div> (200 x 1 200 - varchar)';
+comment on column db2frontend_in.mrpdokumentation_validierung_fe.mrp_dokup_intervention is '<div class=rich-text-field-label><p>Intervention / <span style=font-weight: normal;>Vorschlag zur Fehlervermeldung</span></p></div> (200 x 1 200 - varchar)';
+comment on column db2frontend_in.mrpdokumentation_validierung_fe.mrp_pi_grund_am is '1, (Klare) Indikation nicht (mehr) gegeben (MF) | 2, Verordnung/Dokumentation unvollständig/fehlerhaft (MF) | 3, Ungeeignetes/nicht am besten geeignetes Arzneimittel für die Indikation (MF) | 4, Ungeeignetes/nicht am besten geeignetes Arzneimittel bezüglich Kosten (MF) | 5, Ungeeignetes/nicht am besten geeignetes Arzneimittelform für die Indikation (MF) | 6, Übertragungsfehler (MF) | 7, Substitution aut idem/aut simile (MF) | 11, (Klare) Indikation, aber kein Medikament angeordnet (MF) | 12, Stellfehler (MF) | 13, Arzneimittelallergie oder anamnestische Faktoren nicht berücksichtigt (MF) | 14, Doppelverordnung (MF)<div class=rich-text-field-label><p style=text-align: left;>AM: Arzneimittel</p></div> (10 x 1 10 - numeric)';
 comment on column db2frontend_in.mrpdokumentation_validierung_fe.mrp_pi_grund_anw is '1, Applikation (Dauer) (MF) | 2, Inkompatibilität oder falsche Zubereitung (MF) | 3, Applikation (Art) (MF) | 4, Anfrage zur Administration/KompatibilitätANW: Anwendung (10 x 1 10 - numeric)';
 comment on column db2frontend_in.mrpdokumentation_validierung_fe.mrp_pigrund_d is '1, Kein TDM oder Laborkontrolle durchgeführt oder nicht beachtet (MF) | 2, (Fehlerhafte) Dosis (MF) | 3, (Fehlende) Dosisanpassung (Organfunktion) (MF) | 4, (Fehlerhaftes) Dosisinterval (MF)D: Dosis (10 x 1 10 - numeric)';
 comment on column db2frontend_in.mrpdokumentation_validierung_fe.mrp_pigrund_and is '1, Interaktion (MF) | 2, Kontraindikation (MF) | 3, Nebenwirkungen (10 x 1 10 - numeric)';
@@ -249,8 +250,8 @@ comment on column db2frontend_in.mrpdokumentation_validierung_fe.mrp_ip_klasse i
 comment on column db2frontend_in.mrpdokumentation_validierung_fe.mrp_ip_klasse_disease is 'Disease (200 x 1 200 - varchar)';
 comment on column db2frontend_in.mrpdokumentation_validierung_fe.mrp_ip_klasse_labor is 'Labor (200 x 1 200 - varchar)';
 comment on column db2frontend_in.mrpdokumentation_validierung_fe.mrp_pigrund_s is '1, Beratung/Auswahl eines Arzneistoffs | 2, Beratung/Auswahl zur Dosierung eines Arzneistoffs | 3, Beschaffung/Kosten | 4, Keine Pause von AM, die prä-OP pausiert werden müssen (MF) | 5, Schulung/Beratung eines PatientenS: sonstiges (10 x 1 10 - numeric)';
-comment on column db2frontend_in.mrpdokumentation_validierung_fe.mrp_massn_am is '1, Anweisung für die Applikation geben | 2, Arzneimittel ändern | 3, Arzneimittel stoppen/pausieren | 4, Arzneimittel neu ansetzen | 5, Dosierung ändern | 6, Formulierung ändern | 7, Hilfe bei Beschaffung | 8, Information an Arzt/Pflege | 9, Information an Patient | 10, TDM oder Laborkontrolle emfohlen<div class="rich-text-field-label"><p>AM: Arzneimittel</p></div> (10 x 1 10 - numeric)';
-comment on column db2frontend_in.mrpdokumentation_validierung_fe.mrp_massn_orga is '1, Aushändigung einer Information/eines Medikationsplans | 2, CIRS-/AMK-Meldung | 3, Einbindung anderer Berurfsgruppen z.B. des Stationsapothekers | 4, Etablierung einer Doppelkontrolle | 5, Lieferantenwechsel | 6, Optimierung der internen und externene Kommunikation | 7, Prozessoptimierung/Etablierung einer SOP/VA | 8, Sensibilisierung/Schulung<div class="rich-text-field-label"><p>ORGA: Organisatorisch</p></div> (10 x 1 10 - numeric)';
+comment on column db2frontend_in.mrpdokumentation_validierung_fe.mrp_massn_am is '1, Anweisung für die Applikation geben | 2, Arzneimittel ändern | 3, Arzneimittel stoppen/pausieren | 4, Arzneimittel neu ansetzen | 5, Dosierung ändern | 6, Formulierung ändern | 7, Hilfe bei Beschaffung | 8, Information an Arzt/Pflege | 9, Information an Patient | 10, TDM oder Laborkontrolle emfohlen<div class=rich-text-field-label><p>AM: Arzneimittel</p></div> (10 x 1 10 - numeric)';
+comment on column db2frontend_in.mrpdokumentation_validierung_fe.mrp_massn_orga is '1, Aushändigung einer Information/eines Medikationsplans | 2, CIRS-/AMK-Meldung | 3, Einbindung anderer Berurfsgruppen z.B. des Stationsapothekers | 4, Etablierung einer Doppelkontrolle | 5, Lieferantenwechsel | 6, Optimierung der internen und externene Kommunikation | 7, Prozessoptimierung/Etablierung einer SOP/VA | 8, Sensibilisierung/Schulung<div class=rich-text-field-label><p>ORGA: Organisatorisch</p></div> (10 x 1 10 - numeric)';
 comment on column db2frontend_in.mrpdokumentation_validierung_fe.mrp_notiz is 'Notiz (1000 x 1 1000 - varchar)';
 comment on column db2frontend_in.mrpdokumentation_validierung_fe.mrp_dokup_hand_emp_akz is '1, Arzt / Pflege informiert | 2, Intervention vorgeschlagen und umgesetzt | 3, Intervention vorgeschlagen, nicht umgesetzt (keine Kooperation) | 4, Intervention vorgeschlagen, nicht umgesetzt (Nutzen-Risiko-Abwägung) | 5, Intervention vorgeschlagen, Umsetzung unbekannt | 6, Problem nicht gelöstHandlungsempfehlung akzeptiert? (10 x 1 10 - numeric)';
 comment on column db2frontend_in.mrpdokumentation_validierung_fe.mrp_wiedervorlage is '1, Ja | 0, NeinMRP Wiedervorlage (10 x 1 10 - numeric)';
