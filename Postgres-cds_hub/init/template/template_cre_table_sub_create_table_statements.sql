cc69e617a3469ff8761c4a73eb28e18fee14f14f
Matthias Reusche <matthias.reusche@uni-leipzig.de>
2024-06-04 17:06:08 +0200

    Remove restriction on record comments

--- Postgres-cds_hub/init/template/template_cre_table_sub_create_table_statements.sql
+++ Postgres-cds_hub/init/template/template_cre_table_sub_create_table_statements.sql
@@ -5,5 +5,5 @@ CREATE TABLE IF NOT EXISTS <%OWNER_SCHEMA%>.<%TABLE_NAME%> (
   <%CREATE_TABLE_STATEMENT_COLUMNS%>
   input_datetime timestamp not null default CURRENT_TIMESTAMP,   -- Time at which the data record is inserted
   last_check_datetime timestamp DEFAULT NULL,   -- Time at which data record was last checked
-  current_dataset_status varchar(50) DEFAULT 'input'   -- Processing status of the data record
+  current_dataset_status varchar DEFAULT 'input'   -- Processing status of the data record
 );
