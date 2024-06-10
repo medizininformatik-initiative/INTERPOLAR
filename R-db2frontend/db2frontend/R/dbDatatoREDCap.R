83bc70d96398150462ffa5c82ce895dd905bec56
Matthias Reusche <matthias.reusche@uni-leipzig.de>
2024-06-10 18:38:19 +0200

    FE order of columns is important for data transfer

--- R-db2frontend/db2frontend/R/dbDatatoREDCap.R
+++ R-db2frontend/db2frontend/R/dbDatatoREDCap.R
@@ -54,13 +54,18 @@ copyDB2Redcap <- function() {
                                password = DB_DB2FRONTEND_PASSWORD,
                                schema = DB_DB2FRONTEND_SCHEMA_OUT)
 
-  #get relevant columns for Patient and Fall (Phase 1a of INTERPOLAR)
+  #get relevant columns for Patient and Fall (Phase 1a of INTERPOLAR) - order of columns is important
   dataFromDB <- DBI::dbGetQuery(dbcon,
-      "SELECT patient_fe_id, t1.record_id, pat_id, pat_name, pat_vorname, pat_gebdat, pat_geschlecht, patient_complete
-	, fall_fe_id, fall_id, patient_id_fk, redcap_repeat_instrument, redcap_repeat_instance
+      "SELECT t1.record_id,
+	patient_fe_id, pat_id, pat_name, pat_vorname, pat_gebdat, pat_aktuell_alter, pat_geschlecht, patient_complete
+	, redcap_repeat_instrument, redcap_repeat_instance
+	, patient_id_fk , fall_fe_id, fall_pat_id, fall_id
 	, fall_studienphase, fall_station, fall_aufn_dat, fall_aufn_diag, fall_gewicht_aktuell, fall_gewicht_aktl_einheit
-	, fall_groesse, fall_groesse_einheit, fall_status, fall_ent_dat, fall_complete 
-       FROM v_patient t1, v_fall t2 where t1.pat_id = t2.fall_pat_id")
+	, fall_groesse, fall_groesse_einheit, fall_bmi, fall_nieren_insuf_chron, fall_nieren_insuf_ausmass, fall_nieren_insuf_dialysev
+	, fall_leber_insuf, fall_leber_insuf_ausmass, fall_schwanger_mo, fall_op_geplant, fall_op_dat
+	, fall_status, fall_ent_dat, fall_complete 
+	FROM v_patient t1, v_fall t2 where t1.pat_id = t2.fall_pat_id
+	")
 
   #connect to REDCap project
   redcapcon <- redcapAPI::redcapConnection(url = REDCAP_URL, token = REDCAP_TOKEN)
