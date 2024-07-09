7c4a4ef2fe795ea7ff0ff0c797c1937aa9ce4c98
Matthias Reusche <matthias.reusche@uni-leipzig.de>
2024-07-09 11:13:28 +0200

    Add function to check last check datetime in raw data and take over to core data if data is not chance

---
+++ Postgres-cds_hub/init/template/template_take_over_check_date_function.sql
@@ -0,0 +1,14 @@
+------------------------------
+CREATE OR REPLACE FUNCTION db.<%COPY_FUNC_NAME%>()
+RETURNS VOID AS $$
+BEGIN
+    -- Take over last check datetime Functionname: <%COPY_FUNC_NAME%> - From: <%SCHEMA_2%> -> To: <%OWNER_SCHEMA%>
+<%TEMPLATE_TAKE_OVER_DATE_FUNCTION_SUB_SINGLE_TABLE%>
+END;
+$$ LANGUAGE plpgsql;
+
+-- CopyJob CDS in 2 DB_log
+SELECT cron.schedule('*/1 * * * *', 'SELECT db.<%COPY_FUNC_NAME%>();');
+-----------------------------
+
+
---
+++ Postgres-cds_hub/init/template/template_take_over_check_date_function_sub_single_table.sql
@@ -0,0 +1,18 @@
+    -- Transfer last check date from raw to data - if no data changes have occurred
+    -- from  <%OWNER_SCHEMA%>.<%TABLE_NAME%> to <%SCHEMA_2%>.<%TABLE_NAME_2%>
+    FOR current_record IN (SELECT DISTINCT p.<%TABLE_NAME%>_id id, r.last_check_datetime
+                           FROM <%SCHEMA_2%>.<%TABLE_NAME_2%> p, <%OWNER_SCHEMA%>.<%TABLE_NAME%> r
+                           WHERE p.<%TABLE_NAME%>_id=r.<%TABLE_NAME_2%>_id AND (p.last_check_datetime<r.last_check_datetime OR p.last_check_datetime is null)
+                          )
+        LOOP
+            BEGIN
+		UPDATE <%SCHEMA_2%>.<%TABLE_NAME_2%> target_record
+                SET target_record.last_check_datetime = current_record.last_check_datetime
+                WHERE <%TABLE_NAME%>_id = current_record.ID
+                ;
+            EXCEPTION
+                WHEN OTHERS THEN
+		    NULL;
+            END;
+    END LOOP;
+    -- END <%SIMPLE_TABLE_NAME%>
