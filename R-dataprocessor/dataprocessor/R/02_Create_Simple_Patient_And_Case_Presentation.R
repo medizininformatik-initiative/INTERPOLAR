a7a58a99252aa41b7ba43facbba9c5e8a883d9e3
Alexander Strübing <alexander.struebing@gmail.com>
2024-07-11 09:10:45 +0200

    Add Debug Test Code

--- R-dataprocessor/dataprocessor/R/02_Create_Simple_Patient_And_Case_Presentation.R
+++ R-dataprocessor/dataprocessor/R/02_Create_Simple_Patient_And_Case_Presentation.R
@@ -488,6 +488,32 @@ createFrontendTables <- function() {
 
   # Load the Patient resources from database
   patients_from_database <- getPatientsFromDatabase(pids_per_ward)
+
+  #########################
+  # START: FOR DEBUG ONLY #
+  #########################
+  if (etlutils::isDefinedAndTrue("DEBUG_ADD_MULTIPLE_DIFFERENT_PATIENT_LINES")) {
+    # Add new rows with a changed postal code to test the following code line which should
+    # identify one valid Identifier.value from one patient_id (= source record ID).
+    new_row_count <- 2 # number of rows to add
+    patients_from_database <- etlutils::addEmptyRows(patients_from_database, new_row_count, "end")
+    for (i in 1:new_row_count) {
+      # first duplicate the last 'new_row_count' lines
+      i_source <- nrow(patients_from_database) - (2 * new_row_count) + i
+      i_target <- i_source + new_row_count
+      patients_from_database[i_target] <- patients_from_database[i_source]
+    }
+    # now change the postal code and increase the patient_id = db internal record id number
+    new_column_indices <- (nrow(patients_from_database) - new_row_count + 1):nrow(patients_from_database)
+    patients_from_database[new_column_indices, pat_address_postalcode := etlutils::reverseString(pat_address_postalcode)]
+    patients_from_database[new_column_indices, pat_address_postalcode := etlutils::reverseString("ABC")]
+    patients_from_database[new_column_indices, patient_id := patient_id + 1]
+  }
+  #######################
+  # END: FOR DEBUG ONLY #
+  #######################
+
+
   # check error no Patient exists in the current patinet database table
   if (!nrow(patients_from_database)) { #
     etlutils::catErrorMessage(paste0("No Patient resources found."))
