df2a2fbaf603f5514a26d2afa65a75b148aa7d91
Matthias Reusche <matthias.reusche@uni-leipzig.de>
2024-07-12 16:53:13 +0200

    new templates for frontend view

---
+++ Postgres-cds_hub/init/template/template_cre_view3.sql
@@ -0,0 +1,5 @@
+--Create View for frontend tables for schema <%OWNER_SCHEMA%>
+<%TEMPLATE_CRE_VIEW3_SUB_SINGLE_TABLE%>
+
+--SQL Role for Views in Schema <%OWNER_SCHEMA%>
+<%TEMPLATE_CRE_VIEW3_SUB_GRANT%>
