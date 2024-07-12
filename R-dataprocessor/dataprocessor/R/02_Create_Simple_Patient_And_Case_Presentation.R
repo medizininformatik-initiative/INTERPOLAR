9c1e07128ba2b075096dc533719cc6126bdd6fa3
fschmidt <florian.schmidt@imise.uni-leipzig.de>
2024-07-12 11:03:29 +0200

    Add Debug Test Code

--- R-dataprocessor/dataprocessor/R/02_Create_Simple_Patient_And_Case_Presentation.R
+++ R-dataprocessor/dataprocessor/R/02_Create_Simple_Patient_And_Case_Presentation.R
@@ -492,27 +492,15 @@ createFrontendTables <- function() {
   #########################
   # START: FOR DEBUG ONLY #
   #########################
+  # Set parameter DEBUG_ADD_MULTIPLE_DIFFERENT_PATIENT_LINES = true in dataprocessor_config.toml
+  # Add new rows with a changed postal code to test the following code line which should
+  # identify one valid Identifier.value from one patient_id (= source record ID).
   if (etlutils::isDefinedAndTrue("DEBUG_ADD_MULTIPLE_DIFFERENT_PATIENT_LINES")) {
-    # Add new rows with a changed postal code to test the following code line which should
-    # identify one valid Identifier.value from one patient_id (= source record ID).
-    new_row_count <- 2 # number of rows to add
-    patients_from_database <- etlutils::addEmptyRows(patients_from_database, new_row_count, "end")
-    for (i in 1:new_row_count) {
-      # first duplicate the last 'new_row_count' lines
-      i_source <- nrow(patients_from_database) - (2 * new_row_count) + i
-      i_target <- i_source + new_row_count
-      patients_from_database[i_target] <- patients_from_database[i_source]
-    }
-    # now change the postal code and increase the patient_id = db internal record id number
-    new_column_indices <- (nrow(patients_from_database) - new_row_count + 1):nrow(patients_from_database)
-    patients_from_database[new_column_indices, pat_address_postalcode := etlutils::reverseString(pat_address_postalcode)]
-    patients_from_database[new_column_indices, pat_address_postalcode := etlutils::reverseString("ABC")]
-    patients_from_database[new_column_indices, patient_id := patient_id + 1]
+    patients_from_database <- debugAddMultiplePatientLines(patients_from_database)
   }
   #######################
   # END: FOR DEBUG ONLY #
   #######################
-
 
   # check error no Patient exists in the current patinet database table
   if (!nrow(patients_from_database)) { #
