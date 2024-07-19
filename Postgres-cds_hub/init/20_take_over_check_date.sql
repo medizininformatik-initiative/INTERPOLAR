d4d8bd55bce8e739edc34a0f25fb61d7c600a071
Alexander Strübing <alexander.struebing@gmail.com>
2024-07-19 14:16:15 +0200

    Add take over SQL function for encounter and pids_per_ward tables

--- Postgres-cds_hub/init/20_take_over_check_date.sql
+++ Postgres-cds_hub/init/20_take_over_check_date.sql
@@ -7,10 +7,11 @@ BEGIN
     -- Take over last check datetime Functionname: take_over_last_check_date- From: db_log -> To: db_log
 
     -- Transfer last check date from raw to data - if no data changes have occurred
+
     -- from  db_log.patient_raw to db_log.patient
     FOR current_record IN (SELECT DISTINCT p.patient_id AS id, r.last_check_datetime
                            FROM db_log.patient_raw p, db_log.patient r
-                           WHERE p.patient_id=r.patient_raw_id --AND (p.last_check_datetime<r.last_check_datetime OR p.last_check_datetime is null)
+                           WHERE p.patient_id = r.patient_raw_id --AND (p.last_check_datetime<r.last_check_datetime OR p.last_check_datetime is null)
                           )
         LOOP
             BEGIN
@@ -23,7 +24,43 @@ BEGIN
 		    NULL;
             END;
     END LOOP;
-    -- END <%SIMPLE_TABLE_NAME%>
+    -- END patient
+
+    -- from  db_log.encounter_raw to db_log.encounter
+    FOR current_record IN (SELECT DISTINCT p.encounter_id AS id, r.last_check_datetime
+                           FROM db_log.encounter_raw p, db_log.encounter r
+                           WHERE p.encounter_id = r.encounter_raw_id --AND (p.last_check_datetime<r.last_check_datetime OR p.last_check_datetime is null)
+                          )
+        LOOP
+            BEGIN
+		        UPDATE db_log.encounter_raw target_record
+                SET target_record.last_check_datetime = current_record.last_check_datetime
+                WHERE encounter_id = current_record.ID
+                ;
+            EXCEPTION
+                WHEN OTHERS THEN
+		    NULL;
+            END;
+    END LOOP;
+    -- END encounter
+
+    -- from  db_log.pids_per_ward_raw to db_log.pids_per_ward
+    FOR current_record IN (SELECT DISTINCT p.pids_per_ward_id AS id, r.last_check_datetime
+                           FROM db_log.pids_per_ward_raw p, db_log.pids_per_ward r
+                           WHERE p.pids_per_ward_id = r.pids_per_ward_raw_id --AND (p.last_check_datetime<r.last_check_datetime OR p.last_check_datetime is null)
+                          )
+        LOOP
+            BEGIN
+		        UPDATE db_log.pids_per_ward_raw target_record
+                SET target_record.last_check_datetime = current_record.last_check_datetime
+                WHERE pids_per_ward_id = current_record.ID
+                ;
+            EXCEPTION
+                WHEN OTHERS THEN
+		    NULL;
+            END;
+    END LOOP;
+    -- END pids_per_ward
 
 END;
 $$ LANGUAGE plpgsql;
