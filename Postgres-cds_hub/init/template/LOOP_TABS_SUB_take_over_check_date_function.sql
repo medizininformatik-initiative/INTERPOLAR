76f152cd6178544b7d140df5fd34423a14d58c52
Matthias Reusche <matthias.reusche@uni-leipzig.de>
2024-08-12 18:14:00 +0200

    New take over function fpr FHIR Data beetween raw and core data

--- Postgres-cds_hub/init/template/LOOP_TABS_SUB_take_over_check_date_function.sql
+++ Postgres-cds_hub/init/template/LOOP_TABS_SUB_take_over_check_date_function.sql
@@ -1,19 +1,35 @@
-    -- Transfer last check date from raw to data - if no data changes have occurred
-
+    -- Transfer last check date, last_processing_nr (new) from raw to data - if no data changes have occurred
     -- from  <%OWNER_SCHEMA%>.<%TABLE_NAME%> to <%SCHEMA_2%>.<%TABLE_NAME_2%>
-    FOR current_record IN (SELECT DISTINCT p.<%TABLE_NAME%>_id AS id, r.last_check_datetime
-                           FROM <%SCHEMA_2%>.<%TABLE_NAME_2%> p, <%OWNER_SCHEMA%>.<%TABLE_NAME%> r
-                           WHERE p.<%TABLE_NAME%>_id = r.<%TABLE_NAME%>_id AND (p.last_check_datetime < r.last_check_datetime OR p.last_check_datetime IS NULL)
-                          )
+
+    -- Start <%TABLE_NAME_2%>
+    SELECT MAX(last_processing_nr) INTO max_last_pro_nr FROM <%SCHEMA_2%>.<%TABLE_NAME_2%>;
+
+    -- If new dataimports in raw then set process nr of checking
+    FOR current_record IN (
+    SELECT <%TABLE_NAME%>_id AS id, last_check_datetime AS lcd, current_dataset_status AS cds
+    FROM <%SCHEMA_2%>.<%TABLE_NAME%> WHERE last_processing_nr IN
+        (SELECT last_processing_nr FROM <%SCHEMA_2%>.<%TABLE_NAME%> WHERE <%TABLE_NAME%>_id IN 
+            (SELECT <%TABLE_NAME%>_id FROM <%SCHEMA_2%>.<%TABLE_NAME_2%> WHERE last_processing_nr=max_last_pro_nr
+            )
+         AND last_processing_nr=last_raw_pro_nr
+         )
+    )
         LOOP
             BEGIN
-		        UPDATE <%SCHEMA_2%>.<%TABLE_NAME_2%> target_record
-                SET last_check_datetime = current_record.last_check_datetime
-                WHERE <%TABLE_NAME%>_id = current_record.id
-                ;
+                UPDATE <%SCHEMA_2%>.<%TABLE_NAME_2%>
+                SET last_check_datetime = current_record.lcd
+                , current_dataset_status = current_record.cds
+                , last_processing_nr = new_last_pro_nr
+                WHERE <%TABLE_NAME%>_id = current_record.id;
             EXCEPTION
                 WHEN OTHERS THEN
-		    NULL;
+                    NULL;
             END;
     END LOOP;
-    -- END <%TABLE_NAME%>
+
+    INSERT INTO db_log.data_import_hist (table_primary_key, last_processing_nr, schema_name, table_name, last_check_datetime, current_dataset_status, function_name)
+    ( SELECT <%TABLE_NAME_2%>_id AS table_primary_key, last_processing_nr, '<%SCHEMA_2%>' AS schema_name, '<%TABLE_NAME_2%>' AS table_name, last_check_datetime, current_dataset_status, 'copy_type_cds_in_to_db_log' FROM <%SCHEMA_2%>.<%TABLE_NAME_2%>
+    EXCEPT SELECT table_primary_key, last_processing_nr,schema_name, table_name, last_check_datetime, current_dataset_status, function_name FROM db_log.data_import_hist
+    );
+    -- End <%TABLE_NAME_2%>
+    -----------------------------------------------------------------------------------------------------------------
--- Postgres-cds_hub/init/template/template_take_over_check_date_function.sql
+++ Postgres-cds_hub/init/template/template_take_over_check_date_function.sql
@@ -3,8 +3,15 @@ CREATE OR REPLACE FUNCTION db.<%COPY_FUNC_NAME%>()
 RETURNS VOID AS $$
 DECLARE
     current_record record;
+    new_last_pro_nr INT; -- New processing number for these sync
+    max_last_pro_nr INT; -- Last processing number in core data
+    last_raw_pro_nr INT; -- Last processing number in raw data - last new dataimport (offset)
 BEGIN
-    -- Take over last check datetime Functionname: <%COPY_FUNC_NAME%>- From: <%SCHEMA_2%> -> To: <%OWNER_SCHEMA%>
+    -- Take over last check datetime Functionname: <%COPY_FUNC_NAME%> the last_pro_nr - From: <%SCHEMA_2%> (raw) -> To: <%OWNER_SCHEMA%>
+    SELECT nextval('db.db_seq') INTO new_last_pro_nr; -- Get the processing number for this sync process
+
+    -- Last import Nr in raw-data
+    SELECT MAX(last_processing_nr) INTO last_raw_pro_nr FROM db_log.data_import_hist WHERE table_name like '%_raw' AND schema_name='db_log';
 
 <%LOOP_TABS_SUB_take_over_check_date_function%>
 
