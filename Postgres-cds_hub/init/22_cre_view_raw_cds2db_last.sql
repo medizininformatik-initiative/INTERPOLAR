fbce9c3db404d39242c3071b6418e07043ffba8d
Matthias Reusche <matthias.reusche@uni-leipzig.de>
2024-11-07 13:59:26 +0100

    DB - New view of last raw data to optimize FHIR import

---
+++ Postgres-cds_hub/init/22_cre_view_raw_cds2db_last.sql
@@ -0,0 +1,145 @@
+-- ########################################################################################################
+--
+-- This file is generated. Changes should only be made by regenerating the file.
+--
+-- Rights definition file             : ./Postgres-cds_hub/init/template/User_Schema_Rights_Definition.xlsx
+-- Rights definition file last update : 2024-11-07 11:28:16
+-- Rights definition file size        : 15113 Byte
+--
+-- Create SQL Tables in Schema "cds2db_out"
+-- Create time: 2024-11-07 13:39:47
+-- TABLE_DESCRIPTION:  ./R-cds2db/cds2db/inst/extdata/Table_Description.xlsx[table_description]
+-- SCRIPTNAME:  22_cre_view_raw_cds2db_last.sql
+-- TEMPLATE:  template_cre_view4.sql
+-- OWNER_USER:  cds2db_user
+-- OWNER_SCHEMA:  cds2db_out
+-- TAGS:  
+-- TABLE_PREFIX:  v_
+-- TABLE_POSTFIX:  _lraw
+-- RIGHTS:  SELECT
+-- GRANT_TARGET_USER:  cds2db_user
+-- COPY_FUNC_SCRIPTNAME:  
+-- COPY_FUNC_TEMPLATE:  
+-- COPY_FUNC_NAME:  
+-- SCHEMA_2:  db_log
+-- TABLE_POSTFIX_2:  _raw
+-- SCHEMA_3:  
+-- TABLE_POSTFIX_3:  
+-- ########################################################################################################
+
+--Create SQL View for latest raw-Datasets for schema cds2db_out
+
+CREATE OR REPLACE VIEW cds2db_out.v_encounter_lraw AS (
+SELECT * FROM db_log.encounter_raw
+WHERE TO_CHAR(COALESCE(last_check_datetime, input_datetime),'YYYY-MM-DD HH24:MI') IN (SELECT TO_CHAR(MAX(COALESCE(last_check_datetime, input_datetime)),'YYYY-MM-DD HH24:MI') FROM db_log.encounter_raw)
+);
+
+CREATE OR REPLACE VIEW cds2db_out.v_patient_lraw AS (
+SELECT * FROM db_log.patient_raw
+WHERE TO_CHAR(COALESCE(last_check_datetime, input_datetime),'YYYY-MM-DD HH24:MI') IN (SELECT TO_CHAR(MAX(COALESCE(last_check_datetime, input_datetime)),'YYYY-MM-DD HH24:MI') FROM db_log.patient_raw)
+);
+
+CREATE OR REPLACE VIEW cds2db_out.v_condition_lraw AS (
+SELECT * FROM db_log.condition_raw
+WHERE TO_CHAR(COALESCE(last_check_datetime, input_datetime),'YYYY-MM-DD HH24:MI') IN (SELECT TO_CHAR(MAX(COALESCE(last_check_datetime, input_datetime)),'YYYY-MM-DD HH24:MI') FROM db_log.condition_raw)
+);
+
+CREATE OR REPLACE VIEW cds2db_out.v_medication_lraw AS (
+SELECT * FROM db_log.medication_raw
+WHERE TO_CHAR(COALESCE(last_check_datetime, input_datetime),'YYYY-MM-DD HH24:MI') IN (SELECT TO_CHAR(MAX(COALESCE(last_check_datetime, input_datetime)),'YYYY-MM-DD HH24:MI') FROM db_log.medication_raw)
+);
+
+CREATE OR REPLACE VIEW cds2db_out.v_medicationrequest_lraw AS (
+SELECT * FROM db_log.medicationrequest_raw
+WHERE TO_CHAR(COALESCE(last_check_datetime, input_datetime),'YYYY-MM-DD HH24:MI') IN (SELECT TO_CHAR(MAX(COALESCE(last_check_datetime, input_datetime)),'YYYY-MM-DD HH24:MI') FROM db_log.medicationrequest_raw)
+);
+
+CREATE OR REPLACE VIEW cds2db_out.v_medicationadministration_lraw AS (
+SELECT * FROM db_log.medicationadministration_raw
+WHERE TO_CHAR(COALESCE(last_check_datetime, input_datetime),'YYYY-MM-DD HH24:MI') IN (SELECT TO_CHAR(MAX(COALESCE(last_check_datetime, input_datetime)),'YYYY-MM-DD HH24:MI') FROM db_log.medicationadministration_raw)
+);
+
+CREATE OR REPLACE VIEW cds2db_out.v_medicationstatement_lraw AS (
+SELECT * FROM db_log.medicationstatement_raw
+WHERE TO_CHAR(COALESCE(last_check_datetime, input_datetime),'YYYY-MM-DD HH24:MI') IN (SELECT TO_CHAR(MAX(COALESCE(last_check_datetime, input_datetime)),'YYYY-MM-DD HH24:MI') FROM db_log.medicationstatement_raw)
+);
+
+CREATE OR REPLACE VIEW cds2db_out.v_observation_lraw AS (
+SELECT * FROM db_log.observation_raw
+WHERE TO_CHAR(COALESCE(last_check_datetime, input_datetime),'YYYY-MM-DD HH24:MI') IN (SELECT TO_CHAR(MAX(COALESCE(last_check_datetime, input_datetime)),'YYYY-MM-DD HH24:MI') FROM db_log.observation_raw)
+);
+
+CREATE OR REPLACE VIEW cds2db_out.v_diagnosticreport_lraw AS (
+SELECT * FROM db_log.diagnosticreport_raw
+WHERE TO_CHAR(COALESCE(last_check_datetime, input_datetime),'YYYY-MM-DD HH24:MI') IN (SELECT TO_CHAR(MAX(COALESCE(last_check_datetime, input_datetime)),'YYYY-MM-DD HH24:MI') FROM db_log.diagnosticreport_raw)
+);
+
+CREATE OR REPLACE VIEW cds2db_out.v_servicerequest_lraw AS (
+SELECT * FROM db_log.servicerequest_raw
+WHERE TO_CHAR(COALESCE(last_check_datetime, input_datetime),'YYYY-MM-DD HH24:MI') IN (SELECT TO_CHAR(MAX(COALESCE(last_check_datetime, input_datetime)),'YYYY-MM-DD HH24:MI') FROM db_log.servicerequest_raw)
+);
+
+CREATE OR REPLACE VIEW cds2db_out.v_procedure_lraw AS (
+SELECT * FROM db_log.procedure_raw
+WHERE TO_CHAR(COALESCE(last_check_datetime, input_datetime),'YYYY-MM-DD HH24:MI') IN (SELECT TO_CHAR(MAX(COALESCE(last_check_datetime, input_datetime)),'YYYY-MM-DD HH24:MI') FROM db_log.procedure_raw)
+);
+
+CREATE OR REPLACE VIEW cds2db_out.v_consent_lraw AS (
+SELECT * FROM db_log.consent_raw
+WHERE TO_CHAR(COALESCE(last_check_datetime, input_datetime),'YYYY-MM-DD HH24:MI') IN (SELECT TO_CHAR(MAX(COALESCE(last_check_datetime, input_datetime)),'YYYY-MM-DD HH24:MI') FROM db_log.consent_raw)
+);
+
+CREATE OR REPLACE VIEW cds2db_out.v_location_lraw AS (
+SELECT * FROM db_log.location_raw
+WHERE TO_CHAR(COALESCE(last_check_datetime, input_datetime),'YYYY-MM-DD HH24:MI') IN (SELECT TO_CHAR(MAX(COALESCE(last_check_datetime, input_datetime)),'YYYY-MM-DD HH24:MI') FROM db_log.location_raw)
+);
+
+CREATE OR REPLACE VIEW cds2db_out.v_pids_per_ward_lraw AS (
+SELECT * FROM db_log.pids_per_ward_raw
+WHERE TO_CHAR(COALESCE(last_check_datetime, input_datetime),'YYYY-MM-DD HH24:MI') IN (SELECT TO_CHAR(MAX(COALESCE(last_check_datetime, input_datetime)),'YYYY-MM-DD HH24:MI') FROM db_log.pids_per_ward_raw)
+);
+
+--SQL Role for Views in Schema cds2db_out
+GRANT SELECT ON TABLE cds2db_out.v_encounter_lraw TO cds2db_user;
+GRANT USAGE ON SCHEMA cds2db_out TO cds2db_user;
+
+GRANT SELECT ON TABLE cds2db_out.v_patient_lraw TO cds2db_user;
+GRANT USAGE ON SCHEMA cds2db_out TO cds2db_user;
+
+GRANT SELECT ON TABLE cds2db_out.v_condition_lraw TO cds2db_user;
+GRANT USAGE ON SCHEMA cds2db_out TO cds2db_user;
+
+GRANT SELECT ON TABLE cds2db_out.v_medication_lraw TO cds2db_user;
+GRANT USAGE ON SCHEMA cds2db_out TO cds2db_user;
+
+GRANT SELECT ON TABLE cds2db_out.v_medicationrequest_lraw TO cds2db_user;
+GRANT USAGE ON SCHEMA cds2db_out TO cds2db_user;
+
+GRANT SELECT ON TABLE cds2db_out.v_medicationadministration_lraw TO cds2db_user;
+GRANT USAGE ON SCHEMA cds2db_out TO cds2db_user;
+
+GRANT SELECT ON TABLE cds2db_out.v_medicationstatement_lraw TO cds2db_user;
+GRANT USAGE ON SCHEMA cds2db_out TO cds2db_user;
+
+GRANT SELECT ON TABLE cds2db_out.v_observation_lraw TO cds2db_user;
+GRANT USAGE ON SCHEMA cds2db_out TO cds2db_user;
+
+GRANT SELECT ON TABLE cds2db_out.v_diagnosticreport_lraw TO cds2db_user;
+GRANT USAGE ON SCHEMA cds2db_out TO cds2db_user;
+
+GRANT SELECT ON TABLE cds2db_out.v_servicerequest_lraw TO cds2db_user;
+GRANT USAGE ON SCHEMA cds2db_out TO cds2db_user;
+
+GRANT SELECT ON TABLE cds2db_out.v_procedure_lraw TO cds2db_user;
+GRANT USAGE ON SCHEMA cds2db_out TO cds2db_user;
+
+GRANT SELECT ON TABLE cds2db_out.v_consent_lraw TO cds2db_user;
+GRANT USAGE ON SCHEMA cds2db_out TO cds2db_user;
+
+GRANT SELECT ON TABLE cds2db_out.v_location_lraw TO cds2db_user;
+GRANT USAGE ON SCHEMA cds2db_out TO cds2db_user;
+
+GRANT SELECT ON TABLE cds2db_out.v_pids_per_ward_lraw TO cds2db_user;
+GRANT USAGE ON SCHEMA cds2db_out TO cds2db_user;
+
+
