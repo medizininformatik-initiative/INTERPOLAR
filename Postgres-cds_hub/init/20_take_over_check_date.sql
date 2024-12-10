9deba703d977d94737a16983640b6b3c95ea0852
Matthias Reusche <matthias.reusche@uni-leipzig.de>
2024-12-10 01:48:17 +0100

    DB - Harmonize data type definition in code

--- Postgres-cds_hub/init/20_take_over_check_date.sql
+++ Postgres-cds_hub/init/20_take_over_check_date.sql
@@ -3,11 +3,11 @@
 -- This file is generated. Changes should only be made by regenerating the file.
 --
 -- Rights definition file             : ./Postgres-cds_hub/init/template/User_Schema_Rights_Definition.xlsx
--- Rights definition file last update : 2024-12-05 09:58:05
+-- Rights definition file last update : 2024-12-04 16:58:23
 -- Rights definition file size        : 15179 Byte
 --
 -- Create SQL Tables in Schema "db_log"
--- Create time: 2024-12-05 10:04:11
+-- Create time: 2024-12-10 01:21:48
 -- TABLE_DESCRIPTION:  ./R-cds2db/cds2db/inst/extdata/Table_Description.xlsx[table_description]
 -- SCRIPTNAME:  20_take_over_check_date.sql
 -- TEMPLATE:  template_take_over_check_date_function.sql
@@ -37,25 +37,31 @@ DECLARE
     new_last_pro_nr INT; -- New processing number for these sync
     last_raw_pro_nr INT; -- Last processing number in raw data - last new dataimport (offset)
     max_last_pro_nr INT; -- Last processing number over all entities
-    last_pro_datetime timestamp not null DEFAULT CURRENT_TIMESTAMP; -- Last time function is startet
+    last_pro_datetime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP; -- Last time function is startet
     data_import_hist_every_dataset INT:=0; -- Value for documentation of each individual data record switch off
-    temp varchar; -- Temporary variable for interim results
+    temp VARCHAR; -- Temporary variable for INTerim results
     data_count_pro_all INT:=0; -- Counting all records in this run
     data_count_update INT:=0;
-    timestamp_start varchar;
-    timestamp_end varchar;
+    timestamp_start VARCHAR;
+    timestamp_end VARCHAR;
     tmp_sec double precision:=0; -- Temporary variable to store execution time
-    err_section varchar;
-    err_schema varchar;
-    err_table varchar;
-    err_pid varchar;
+    err_section VARCHAR;
+    err_schema VARCHAR;
+    err_table VARCHAR;
+    err_pid VARCHAR;
+    erg VARCHAR;
 BEGIN
     -- Take over last check datetime Functionname: take_over_last_check_date the last_pro_nr - From: db_log (raw) -> To: db_log
    
     -- set start time
     err_section:='HEAD-01';    err_schema:='db_config';    err_table:='db_process_control';
-	SELECT res FROM public.pg_background_result(public.pg_background_launch('SELECT to_char(CURRENT_TIMESTAMP,''YYYY-MM-DD HH24:MI:SS.US'')'))  AS t(res TEXT) INTO timestamp_start;
-    err_pid:=public.pg_background_launch('UPDATE db_config.db_process_control SET pc_value=to_char(CURRENT_TIMESTAMP,''YYYY-MM-DD HH24:MI:SS.US'')||'' take_over_last_check_date'' WHERE pc_name=''timepoint_1_cron_job_data_transfer''');
+	SELECT res FROM public.pg_background_result(public.pg_background_launch(
+    'SELECT to_char(CURRENT_TIMESTAMP,''YYYY-MM-DD HH24:MI:SS.US'')'
+    ))  AS t(res TEXT) INTO timestamp_start;
+    
+    SELECT res FROM public.pg_background_result(public.pg_background_launch(
+    'UPDATE db_config.db_process_control SET pc_value=to_char(CURRENT_TIMESTAMP,''YYYY-MM-DD HH24:MI:SS.US'')||'' take_over_last_check_date'' WHERE pc_name=''timepoint_1_cron_job_data_transfer'''
+    ) ) AS t(res TEXT) INTO erg;
 
     -- Last import Nr in raw-data
     err_section:='HEAD-05';    err_schema:='db_log';    err_table:='data_import_hist';
@@ -1090,7 +1096,9 @@ BEGIN
     IF data_count_pro_all>0 THEN
         -- calculation of the time period
         err_section:='BOTTOM-01';    err_schema:='/';    err_table:='/';
-        SELECT res FROM pg_background_result(pg_background_launch('SELECT to_char(CURRENT_TIMESTAMP,''YYYY-MM-DD HH24:MI:SS.US'')'))  AS t(res TEXT) INTO timestamp_end;
+        SELECT res FROM pg_background_result(pg_background_launch(
+        'SELECT to_char(CURRENT_TIMESTAMP,''YYYY-MM-DD HH24:MI:SS.US'')'
+        ))  AS t(res TEXT) INTO timestamp_end;
     
         err_section:='BOTTOM-05';    err_schema:='/';    err_table:='/';
         SELECT EXTRACT(EPOCH FROM (to_timestamp(timestamp_end,'YYYY-MM-DD HH24:MI:SS.US') - to_timestamp(timestamp_start,'YYYY-MM-DD HH24:MI:SS.US'))), ' '||timestamp_start||' o '||timestamp_end INTO tmp_sec, temp;
@@ -1106,13 +1114,13 @@ BEGIN
 EXCEPTION
     WHEN OTHERS THEN
     SELECT db.error_log(
-        err_schema => CAST(err_schema AS varchar),                    -- err_schema (varchar) Schema, in dem der Fehler auftrat
-        err_objekt => CAST('db.take_over_last_check_date()' AS varchar),     -- err_objekt (varchar) Objekt (Tabelle, Funktion, etc.)
-        err_user => CAST(current_user AS varchar),                    -- err_user (varchar) Benutzer (kann durch current_user ersetzt werden)
-        err_msg => CAST(SQLSTATE || ' - ' || SQLERRM AS varchar),     -- err_msg (varchar) Fehlernachricht
-        err_line => CAST(err_section AS varchar),                     -- err_line (varchar) Zeilennummer oder Abschnitt
-        err_variables => CAST('Tab: ' || err_table AS varchar),       -- err_variables (varchar) Debug-Informationen zu Variablen
-        last_processing_nr => CAST(new_last_pro_nr AS int)            -- last_processing_nr (int) Letzte Verarbeitungsnummer - wenn vorhanden
+        err_schema => CAST(err_schema AS VARCHAR),                    -- err_schema (VARCHAR) Schema, in dem der Fehler auftrat
+        err_objekt => CAST('db.take_over_last_check_date()' AS VARCHAR),     -- err_objekt (VARCHAR) Objekt (Tabelle, Funktion, etc.)
+        err_user => CAST(current_user AS VARCHAR),                    -- err_user (VARCHAR) Benutzer (kann durch current_user ersetzt werden)
+        err_msg => CAST(SQLSTATE || ' - ' || SQLERRM AS VARCHAR),     -- err_msg (VARCHAR) Fehlernachricht
+        err_line => CAST(err_section AS VARCHAR),                     -- err_line (VARCHAR) Zeilennummer oder Abschnitt
+        err_variables => CAST('Tab: ' || err_table||' e:'||erg AS VARCHAR),       -- err_variables (VARCHAR) Debug-Informationen zu Variablen
+        last_processing_nr => CAST(new_last_pro_nr AS INT)            -- last_processing_nr (int) Letzte Verarbeitungsnummer - wenn vorhanden
     ) INTO temp;
 
     RETURN 'Fehler db.take_over_last_check_date - '||SQLSTATE||' - last_raw_pro_nr:'||last_raw_pro_nr;
