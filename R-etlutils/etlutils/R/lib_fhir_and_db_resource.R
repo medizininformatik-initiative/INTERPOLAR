b7a99febfe109496fc0e61a92279f6659245da4c
astruebi <alexander.struebing@gmail.com>
2025-02-27 15:49:29 +0100

    Add convenience function for IDs <-> Refs

--- R-etlutils/etlutils/R/lib_fhir_and_db_resource.R
+++ R-etlutils/etlutils/R/lib_fhir_and_db_resource.R
@@ -84,3 +84,54 @@ getEncIDColumn <- function(resource_name) {
   enc_id_column <- paste0(getResourceAbbreviation(resource_name), "_", enc_id_column)
   return(enc_id_column)
 }
+
+#' Generate FHIR resource references
+#'
+#' This function generates a valid FHIR resource reference by combining a given `reference_type`
+#' (e.g., `"Patient"`, `"Encounter"`) with a cleaned FHIR ID. The ID is extracted using
+#' `getAfterLastSlash()`, ensuring that any existing reference structure is properly handled.
+#'
+#' @param reference_type A character string specifying the type of FHIR resource
+#'                       (e.g., `"Patient"`, `"Encounter"`).
+#' @param fhir_id_or_reference A character string containing either a full FHIR reference
+#'                              or just an ID. If a full reference is provided, only the
+#'                              ID after the last slash is used.
+#'
+#' @return A character string representing the correctly formatted FHIR reference
+#'         in the form `"<reference_type>/<fhir_id>"`.
+#'
+#' @export
+getFHIRReference <- function(reference_type, fhir_id_or_reference) {
+  fhir_id <- getAfterLastSlash(fhir_id_or_reference)
+  return(paste0(reference_type, "/", fhir_id))
+}
+
+#' Generate a FHIR Patient reference
+#'
+#' This function generates a valid FHIR Patient reference by extracting the patient ID
+#' from a given input and appending it to `"Patient/"`.
+#'
+#' @param patient_id_or_reference A character string containing a full FHIR reference
+#'                                 or just a patient ID.
+#'
+#' @return A character string representing the correctly formatted FHIR Patient reference.
+#'
+#' @export
+getFHIRPatientReference <- function(patient_id_or_reference) {
+  getFHIRReference("Patient", patient_id_or_reference)
+}
+
+#' Generate a FHIR Encounter reference
+#'
+#' This function generates a valid FHIR Encounter reference by extracting the encounter ID
+#' from a given input and appending it to `"Encounter/"`.
+#'
+#' @param encounter_id_or_reference A character string containing a full FHIR reference
+#'                                   or just an encounter ID.
+#'
+#' @return A character string representing the correctly formatted FHIR Encounter reference.
+#'
+#' @export
+getFHIREncounterReference <- function(encounter_id_or_reference) {
+  getFHIRReference("Encounter", encounter_id_or_reference)
+}
