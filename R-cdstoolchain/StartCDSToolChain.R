eea445ba3b65d299b4b50019df361d19687409c1
astruebi <alexander.struebing@gmail.com>
2025-03-10 20:54:00 +0100

    Update full project structure

--- R-cdstoolchain/StartCDSToolChain.R
+++ R-cdstoolchain/StartCDSToolChain.R
@@ -22,28 +22,49 @@ for (i in seq_along(DEBUG_DATES)) {
     options(error = NULL)
     # browser()
     cds2db::retrieve()
-    # browser()
     dataprocessor::processData()
-    # browser()
     db2frontend::startDB2Frontend()
   }, error = function(e) {
-    # Zerlege die Fehlermeldung in einzelne Zeilen
+    # Split the error message into individual lines
     error_lines <- unlist(strsplit(e$message, "\n"))
 
-    # Definiere das Muster für die SQL-Spaltenfehler
+    # Define the pattern for SQL column errors
     allowed_pattern <- "column .* of relation .* does not exist"
 
-    # Prüfe, ob eine der Zeilen das Muster enthält
+    # Check if any of the lines contain the pattern
     if (any(grepl(allowed_pattern, error_lines))) {
       message("Ignoring expected error: ", e$message)
 
-      # `next` nur ausführen, wenn nicht im letzten Schleifendurchlauf
+      # Execute `next` only if not in the last iteration of the loop
       if (i < length(DEBUG_DATES)) {
         next
       }
     } else {
-      stop(e)  # Andere Fehler abbrechen
+      stop(e)  # Abort on other errors
     }
   })
   cat("END DEBUG_DAY", DEBUG_DAY, "\n")
 }
+
+#'
+#' Loads all tables from subdirectory 'tables' of the last directory
+#' in the given tale_path.
+#'
+loadTables <- function(table_path, name_pattern = NA) {
+  table_names <- list.files(path = table_path, pattern = ".\\.RData$")
+  if (!is.na(name_pattern)) {
+    table_names <- table_names[grep(name_pattern, table_names, ignore.case = TRUE)]
+  }
+  tables <- list()
+  for (table_name in table_names) {
+    full_table_path <- paste0(table_path, table_name)
+    table <- try(readRDS(full_table_path), silent = TRUE)
+    if (data.table::is.data.table(table)) {
+      tables <- append(tables, list(table))
+      names(tables)[length(tables)] <- gsub("\\.RData$", "", table_name)
+    }
+  }
+  return(tables)
+}
+
+#tables <- loadTables("./outputLocal/db2frontend/tables/")
