62cc5e4739c07f687cac0c9ccf182148c22de28c
astruebi <alexander.struebing@gmail.com>
2025-03-14 16:19:24 +0100

    Rename Debug Parameter

--- R-cds2db/cds2db/R/02_Get_PIDs.R
+++ R-cds2db/cds2db/R/02_Get_PIDs.R
@@ -261,7 +261,7 @@ getEncounters <- function(table_description, current_datetime) {
       )
 
       # stop the execution and print the current result of FHIR search request (DEBUG)
-      etlutils::checkDebugTestError("DEBUG_ENCOUNTER_REQUEST_TEST", request_encounter)
+      etlutils::checkDebugTestError("DEBUG_FHIR_SEARCH_ENCOUNTER_REQUEST_TEST", request_encounter)
 
       table_enc <- etlutils::downloadAndCrackFHIRResources(request = request_encounter,
                                                            table_description = table_description,
