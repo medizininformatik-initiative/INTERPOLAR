5164602a8f3f5f7128f51070aecf27f21a84f42e
Matthias Reusche <matthias.reusche@uni-leipzig.de>
2025-04-07 00:55:08 +0200

    DB - db config objects (020) and parameters (030) implementation of the scripts to migration scripts Refs #685

--- Postgres-cds_hub/init/030_db_parameter.sql
+++ Postgres-cds_hub/init/030_db_parameter.sql
@@ -1,34 +1,95 @@
 -- ###################### PARAMETER ############################
--- >>>> WICHTIG Standort / Zeitabhängig <<<<
-insert into db_config.db_parameter (parameter_name, parameter_value, parameter_description)
-values ('release_version','2.10','GitHup release version');
---update db_config.db_parameter set parameter_value='?' where parameter_name='release_version';
+DO
+$$
+DECLARE
+---------------------------------------------------------------------------------------
+   -- >>>> WICHTIG Standort / Zeitabhängig <<<< -- Hier bitte Variablen setzen
+   release_version VARCHAR := '2.10';               -- GitHup release version
+   release_version_date VARCHAR := '2025-04-04'     -- GitHup release version date
+   project_participants VARCHAR := 'IMISE Leipzig'  -- project participants to distinguish between different instances
+   pause_after_process_execution VARCHAR := '10'    -- Pause after copy process execution in second [5-30 sec] - ready to connect
+   data_import_hist_every_dataset VARCHAR := 'no'   -- Documentation of each individual data record (db_log.data_import_hist) in all the transfer functions [yes|no] - large resource requirements only for debugging
+   max_process_time_set_ready VALUES := '120'       -- Maximum time that the semaphore may remain in use before it is released again in minutes [5-120]
+   copy_fhir_metadata_from_raw_to_typed VARCHAR := 'N' -- Optionale Einstellung (Yes/No) ob die FHIR-Metadaten beim Kopiervorgang auch von RAW in die TYPED Tabellen übernommen werden soll.
+---------------------------------------------------------------------------------------
+BEGIN
+   IF NOT EXISTS (
+      SELECT 1 FROM db_config.db_parameter WHERE pc_name = 'release_version'
+   ) THEN
+      INSERT INTO db_config.db_parameter (parameter_name, parameter_value, parameter_description)
+      VALUES ('release_version',release_version,'GitHup release version');
+   ELSE
+      UPDATE db_config.db_parameter SET parameter_value=release_version WHERE parameter_name='release_version';
+   END IF;
 
-insert into db_config.db_parameter (parameter_name, parameter_value, parameter_description)
-values ('release_version_date','2025-04-04','GitHup release version');
---update db_config.db_parameter set parameter_value='YYYY-MM-DD' where parameter_name='release_version_date';
+   IF NOT EXISTS (
+      SELECT 1 FROM db_config.db_parameter WHERE pc_name = 'release_version_date'
+   ) THEN
+      INSERT INTO db_config.db_parameter (parameter_name, parameter_value, parameter_description)
+      VALUES ('release_version_date',release_version_date,'GitHup release version date');
+   ELSE
+      UPDATE db_config.db_parameter SET parameter_value=release_version_date WHERE parameter_name='release_version_date';
+   END IF;
 
-insert into db_config.db_parameter (parameter_name, parameter_value, parameter_description)
-values ('project_participants','IMISE Leipzig','project participants to distinguish between different instances');
---update db_config.db_parameter set parameter_value='?' where parameter_name='project_participants';
--- >>>> WICHTIG <<<<
+   IF NOT EXISTS (
+      SELECT 1 FROM db_config.db_parameter WHERE pc_name = 'project_participants'
+   ) THEN
+      INSERT INTO db_config.db_parameter (parameter_name, parameter_value, parameter_description)
+      VALUES ('project_participants',release_version_date,'project participants to distinguish between different instances');
+   ELSE
+      UPDATE db_config.db_parameter SET parameter_value=release_version_date WHERE parameter_name='project_participants';
+   END IF;
 
-insert into db_config.db_parameter (parameter_name, parameter_value, parameter_description)
-values ('database_initialization_time',to_char(CURRENT_TIMESTAMP,'YYYY-MM-DD HH24:MI:SS'),'Timestamp of database initialization');
---update db_config.db_parameter set parameter_value='?' where parameter_name='database_initialization_time';
+   IF NOT EXISTS (
+      SELECT 1 FROM db_config.db_parameter WHERE pc_name = 'database_initialization_time'
+   ) THEN
+      INSERT INTO db_config.db_parameter (parameter_name, parameter_value, parameter_description)
+      VALUES ('database_initialization_time',to_char(CURRENT_TIMESTAMP,'YYYY-MM-DD HH24:MI:SS'),'Timestamp of database initialization');
+   END IF;
 
-insert into db_config.db_parameter (parameter_name, parameter_value, parameter_description)
-values ('pause_after_process_execution','10','Pause after copy process execution in second [5-30 sec]');
---update db_config.db_parameter set parameter_value='?' where parameter_name='pause_after_process_execution';
+   IF NOT EXISTS (
+      SELECT 1 FROM db_config.db_parameter WHERE pc_name = 'last_migration_date'
+   ) THEN
+      INSERT INTO db_config.db_parameter (parameter_name, parameter_value, parameter_description)
+      VALUES ('last_migration_date',to_char(CURRENT_TIMESTAMP,'YYYY-MM-DD HH24:MI:SS'),'last_migration_date');
+   ELSE
+      UPDATE db_config.db_parameter SET parameter_value=to_char(CURRENT_TIMESTAMP,'YYYY-MM-DD HH24:MI:SS') WHERE parameter_name='last_migration_date';
+   END IF;
 
-insert into db_config.db_parameter (parameter_name, parameter_value, parameter_description)
-values ('data_import_hist_every_dataset','no','Documentation of each individual data record (db_log.data_import_hist) in all the transfer functions [yes|no]');
---update db_config.db_parameter set parameter_value='?' where parameter_name='data_import_hist_every_dataset';
+   IF NOT EXISTS (
+      SELECT 1 FROM db_config.db_parameter WHERE pc_name = 'pause_after_process_execution'
+   ) THEN
+      INSERT INTO db_config.db_parameter (parameter_name, parameter_value, parameter_description)
+      VALUES ('pause_after_process_execution',pause_after_process_execution,'Pause after copy process execution in second [5-30 sec] - ready to connect');
+   ELSE
+      UPDATE db_config.db_parameter SET parameter_value=pause_after_process_execution WHERE parameter_name='pause_after_process_execution';
+   END IF;
 
-insert into db_config.db_parameter (parameter_name, parameter_value, parameter_description)
-values ('max_process_time_set_ready','120','Maximum time that the semaphore may remain in use before it is released again in minutes [5-120]');
---update db_config.db_parameter set parameter_value='?' where parameter_name='max_process_time_set_ready';
+   IF NOT EXISTS (
+      SELECT 1 FROM db_config.db_parameter WHERE pc_name = 'data_import_hist_every_dataset'
+   ) THEN
+      INSERT INTO db_config.db_parameter (parameter_name, parameter_value, parameter_description)
+      VALUES ('data_import_hist_every_dataset',data_import_hist_every_dataset,'Documentation of each individual data record (db_log.data_import_hist) in all the transfer functions [yes|no] - large resource requirements only for debugging');
+   ELSE
+      UPDATE db_config.db_parameter SET parameter_value=data_import_hist_every_dataset WHERE parameter_name='data_import_hist_every_dataset';
+   END IF;
 
-insert into db_config.db_parameter (parameter_name, parameter_value, parameter_description)
-values ('copy_fhir_metadata_from_raw_to_typed','N','Optionale Einstellung (Yes/No) ob die FHIR-Metadaten beim Kopiervorgang auch von RAW in die TYPED Tabellen übernommen werden soll.');
---update db_config.db_parameter set parameter_value='?' where parameter_name='copy_fhir_metadata_from_raw_to_typed';
+   IF NOT EXISTS (
+      SELECT 1 FROM db_config.db_parameter WHERE pc_name = 'max_process_time_set_ready'
+   ) THEN
+      INSERT INTO db_config.db_parameter (parameter_name, parameter_value, parameter_description)
+      VALUES ('max_process_time_set_ready',max_process_time_set_ready,'Maximum time that the semaphore may remain in use before it is released again in minutes [5-120]');
+   ELSE
+      UPDATE db_config.db_parameter SET parameter_value=max_process_time_set_ready WHERE parameter_name='max_process_time_set_ready';
+   END IF;
+
+   IF NOT EXISTS (
+      SELECT 1 FROM db_config.db_parameter WHERE pc_name = 'copy_fhir_metadata_from_raw_to_typed'
+   ) THEN
+      INSERT INTO db_config.db_parameter (parameter_name, parameter_value, parameter_description)
+      VALUES ('copy_fhir_metadata_from_raw_to_typed',copy_fhir_metadata_from_raw_to_typed,'Optionale Einstellung (Yes/No) ob die FHIR-Metadaten beim Kopiervorgang auch von RAW in die TYPED Tabellen übernommen werden soll.');
+   ELSE
+      UPDATE db_config.db_parameter SET parameter_value=max_process_time_set_ready WHERE parameter_name='copy_fhir_metadata_from_raw_to_typed';
+   END IF;
+END
+$$;
