a7b3ec69fbdf461718bd2263671366b4702c04e7
Matthias Reusche <matthias.reusche@uni-leipzig.de>
2025-04-07 12:03:01 +0200

    DB - test sema end skipt 999

--- Postgres-cds_hub/init/999_start_semapore_after_migration.sql
+++ Postgres-cds_hub/init/999_start_semapore_after_migration.sql
@@ -4,14 +4,14 @@ BEGIN
 -- Set semapore if exit --------------------------------------------------------------------------
    IF EXISTS (
       SELECT 1 
-      FROM pg_catalog.pg_columns 
+      FROM information_schema.columns
       WHERE table_schema = 'db_config' 
         AND table_name = 'db_process_control' 
         AND column_name = 'pc_name'
    ) THEN
    IF EXISTS (
          SELECT 1 
-         FROM pg_catalog.pg_columns 
+         FROM information_schema.columns
          WHERE table_schema = 'db_config' 
            AND table_name = 'db_process_control' 
            AND column_name = 'pc_value'
@@ -19,5 +19,6 @@ BEGIN
          UPDATE db_config.db_process_control SET pc_value='WaitForCronJob', last_change_timestamp=CURRENT_TIMESTAMP WHERE pc_name='semaphor_cron_job_data_transfer' and pc_value!='WaitForCronJob';
       END IF;
    END IF;
+   UPDATE db_config.db_parameter SET parameter_value=to_char(CURRENT_TIMESTAMP,'YYYY-MM-DD HH24:MI:SS') WHERE parameter_name='last_migration_date';
 END
 $$;
