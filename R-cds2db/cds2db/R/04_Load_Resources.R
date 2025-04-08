98ecb4a24db528f4b6672d7cbf81ee0e742b06bc
Alexander Strübing <a.leipzig@gmx.de>
2025-04-08 13:36:49 +0200

    Add parameter to download FHIR resources by subject= again
    refs #696
    The default (if this new parameter is not defined or false) loads all PID dependent FHIR resources by patient=

--- R-cds2db/cds2db/R/04_Load_Resources.R
+++ R-cds2db/cds2db/R/04_Load_Resources.R
@@ -285,8 +285,15 @@ loadResourcesByPatientIDFromFHIRServer <- function(patient_ids_per_ward, table_d
                     suffix = "\n",
                     na_replacement = "Not present in DB")
 
+  # the parameter FHIR_SEARCH_PIDS_BY_SUBJECT decides if the patient IDs are
+  # passed by subject or patient in the FHIR search request
+  id_param_str <- ifelse (etlutils::isDefinedAndTrue("FHIR_SEARCH_PIDS_BY_SUBJECT"), "subject", "patient")
+
   # Load all data of relevant patients from FHIR server
-  resource_tables_fhir <- etlutils::fhirLoadMultipleResourcesByPID(pids_with_last_updated, table_descriptions, resources_add_search_parameter)
+  resource_tables_fhir <- etlutils::fhirLoadMultipleResourcesByPID(pids_with_last_updated,
+                                                                   table_descriptions,
+                                                                   id_param_str,
+                                                                   resources_add_search_parameter)
 
   raw_fhir_resources <- resource_tables_fhir$raw_fhir_resources
   # The pids_with_last_updated now only contains persons who were older than MIN_PATIENT_AGE at
