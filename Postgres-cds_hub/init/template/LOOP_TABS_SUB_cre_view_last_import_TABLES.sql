adbd92f2a1a443a466999adb568cec5126a4ce18
Matthias Reusche <matthias.reusche@uni-leipzig.de>
2025-05-21 16:07:47 +0200

    DB - sql templates to last update time and improved precision of the view to display the data of the last import Ref #777

--- Postgres-cds_hub/init/template/LOOP_TABS_SUB_cre_view_last_import_TABLES.sql
+++ Postgres-cds_hub/init/template/LOOP_TABS_SUB_cre_view_last_import_TABLES.sql
@@ -1,5 +1,5 @@
 
 CREATE OR REPLACE VIEW <%OWNER_SCHEMA%>.<%TABLE_NAME%> AS (
 SELECT * FROM <%SCHEMA_2%>.<%TABLE_NAME_2%>
-WHERE TO_CHAR(COALESCE(last_check_datetime, input_datetime),'YYYY-MM-DD HH24:MI') IN (SELECT TO_CHAR(MAX(COALESCE(last_check_datetime, input_datetime)),'YYYY-MM-DD HH24:MI') FROM <%SCHEMA_2%>.<%TABLE_NAME_2%>)
+WHERE TO_CHAR(COALESCE(last_check_datetime, input_datetime),'YYYY-MM-DD HH24:MI:SS') IN (SELECT TO_CHAR(MAX(COALESCE(last_check_datetime, input_datetime)),'YYYY-MM-DD HH24:MI:SS') FROM <%SCHEMA_2%>.<%TABLE_NAME_2%>)
 );
--- Postgres-cds_hub/init/template/LOOP_TABS_SUB_take_over_check_date_function_count.sql
+++ Postgres-cds_hub/init/template/LOOP_TABS_SUB_take_over_check_date_function_count.sql
@@ -1,8 +1,5 @@
-    ---- Start check <%SCHEMA_2%>.<%TABLE_NAME%> ----
-    err_section:='CHECK-15';    err_schema:='<%SCHEMA_2%>';    err_table:='<%SCHEMA_2%>.<%TABLE_NAME%>';
-    SELECT COALESCE(MAX(last_processing_nr),0) INTO max_ent_pro_nr FROM <%SCHEMA_2%>.<%TABLE_NAME_2%>;
-    IF COALESCE(max_ent_pro_nr,0)>COALESCE(max_last_pro_nr,0) THEN max_last_pro_nr:=COALESCE(max_ent_pro_nr,0); END IF;
-
+    ---- Start check <%SCHEMA_2%>.<%TABLE_NAME%> - count ----
+    err_section:='CHECK-16';    err_schema:='<%SCHEMA_2%>';    err_table:='<%SCHEMA_2%>.<%TABLE_NAME%>';
     IF data_count_pro_all=0 AND COALESCE(max_ent_pro_nr,0)!=0 THEN -- Nur wenn bisher keine Datensätze gefunden wurden diese Entität überprüfen - sobald eine E. gefunden wurde über alle berechnen
         SELECT COUNT(1) INTO data_count_pro_all
     	FROM <%SCHEMA_2%>.<%TABLE_NAME%> r, <%SCHEMA_2%>.<%TABLE_NAME%> r2, <%SCHEMA_2%>.<%TABLE_NAME_2%> t
@@ -13,5 +10,5 @@
 --/*Test*/ 'INSERT INTO db.data_import_hist (function_name, table_name, schema_name, variable_name ) VALUES ( ''take_over_check_data'', ''<%TABLE_NAME%>'', ''<%SCHEMA_2%>'', ''max_ent_pro_nr / data_count_pro_all :'||max_ent_pro_nr||' / '||data_count_pro_all||''' );'
 --/*Test*/))  AS t(res TEXT) INTO erg;
     END IF;
-    ---- End check <%SCHEMA_2%>.<%TABLE_NAME_2%> ----
+    ---- End check <%SCHEMA_2%>.<%TABLE_NAME_2%> - count ----
 
