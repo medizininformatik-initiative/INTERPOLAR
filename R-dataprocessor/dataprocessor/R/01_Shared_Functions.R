c087c9a68585c1d83845d163e4b4d534c343f7bc
fschmidt <florian.schmidt@imise.uni-leipzig.de>
2025-05-28 13:04:21 +0200

    Reactivate getCurrentDatetime() function

--- R-dataprocessor/dataprocessor/R/01_Shared_Functions.R
+++ R-dataprocessor/dataprocessor/R/01_Shared_Functions.R
@@ -24,6 +24,33 @@ parseQueryList <- function(list_string, split = " ") {
   etlutils::fhirdbGetQueryList(splitted)
 }
 
+#' Get the most relevant current datetime
+#'
+#' This function retrieves the latest encounter end datetime from the `encounters` table.
+#' If no valid end datetime is available, it defaults to the current system time (`Sys.time()`).
+#' The result is converted to a `POSIXct` object with the appropriate timezone.
+#'
+#' NOTE: If the cds2db modules runs with a dubug start and end date, then there can
+#' be encounters with a start date after the end date of another encounter. This should
+#' never happens in a non debug run and can lead to irregularities in the results.
+#'
+#' @param encounters A `data.table` or `data.frame` containing encounter records,
+#'                   where `enc_period_end` represents the encounter end timestamps.
+#'
+#' @return A `POSIXct` object representing the most recent encounter end datetime
+#'         or the current system time if no valid datetime is found.
+#'
+getCurrentDatetime <- function(encounters) {
+  encounters_end <- na.omit(encounters$enc_period_end)
+  sys_time <- Sys.time()
+  datetime <- if (length(encounters_end)) max(encounters_end) - 1 else sys_time
+  if (datetime > sys_time) {
+    # if the latest encounter end is in the future, use the current system time
+    datetime <- sys_time
+  }
+  return(etlutils::as.POSIXctWithTimezone(datetime))
+}
+
 #' Format datetime for SQL queries for Observations.
 #'
 #' This function formats the datetime returned by `getCurrentDatetime()` into an SQL-compatible
