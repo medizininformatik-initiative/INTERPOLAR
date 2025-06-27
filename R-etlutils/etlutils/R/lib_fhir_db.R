5fe8efb110351a9300f6f765d2de86f495103e3d
Alexander Strübing <a.leipzig@gmx.de>
2025-06-27 14:04:34 +0200

    Fix function to create ID lists for SQL queries

--- R-etlutils/etlutils/R/lib_fhir_db.R
+++ R-etlutils/etlutils/R/lib_fhir_db.R
@@ -101,7 +101,7 @@ fhirdbGetEncIDColumn <- function(resource_name) {
 fhirdbGetQueryList <- function(collection, remove_ref_type = FALSE, return_NA_if_empty = FALSE) {
   collection <- unique(na.omit(collection))
   if (!length(collection)) {
-    return(ifelse(return_NA_if_empty, NA, "''"))
+    return(ifelse(return_NA_if_empty, NA, "('')"))
   }
   if (remove_ref_type) {
     collection <- etlutils::fhirdataExtractIDs(collection)
