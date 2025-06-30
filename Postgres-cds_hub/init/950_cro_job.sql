e60f558822555ce829f579dae53eff1f1d888603
Matthias Reusche <matthias.reusche@uni-leipzig.de>
2025-06-30 10:01:17 +0200

    DB - Changing the condition check to create cron job

--- Postgres-cds_hub/init/950_cro_job.sql
+++ Postgres-cds_hub/init/950_cro_job.sql
@@ -265,13 +265,14 @@ $$ LANGUAGE plpgsql; -- db.cron_job_data_transfer
 
 DO
 $$
+DECLARE
+ i INT;
 BEGIN
-    IF NOT EXISTS ( -- if cron job not exist
-            select 1 from cron.job j where active is true and command='SELECT db.cron_job_data_transfer();'
-        ) THEN
-            -- Datatransfer Job anlegen
-            SELECT cron.schedule('*/1 * * * *', 'SELECT db.cron_job_data_transfer();');
-        END IF;
+    SELECT COUNT(1) INTO i FROM cron.job j WHERE active IS TRUE AND command='SELECT db.cron_job_data_transfer();'
+    IF i = 0 THEN
+        -- Datatransfer Job anlegen
+        SELECT cron.schedule('*/1 * * * *', 'SELECT db.cron_job_data_transfer();');
+    END IF;
 END
 $$;
 
