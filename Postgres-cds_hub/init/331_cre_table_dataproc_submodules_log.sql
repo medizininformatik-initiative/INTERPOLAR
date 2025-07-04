2fb6c606b8d1709d270681d4eabfe70b2c26e1a1
Alexander Strübing <a.leipzig@gmx.de>
2025-07-04 14:40:41 +0200

    Update dataprocessor submodules DB column description

--- Postgres-cds_hub/init/331_cre_table_dataproc_submodules_log.sql
+++ Postgres-cds_hub/init/331_cre_table_dataproc_submodules_log.sql
@@ -3,11 +3,11 @@
 -- This file is generated. Changes should only be made by regenerating the file.
 --
 -- Rights definition file             : ./Postgres-cds_hub/init/template/User_Schema_Rights_Definition.xlsx
--- Rights definition file last update : 2025-06-23 09:23:55
+-- Rights definition file last update : 2025-07-02 16:19:58
 -- Rights definition file size        : 16391 Byte
 --
 -- Create SQL Tables in Schema "db_log"
--- Create time: 2025-06-23 09:28:47
+-- Create time: 2025-07-04 14:36:18
 -- TABLE_DESCRIPTION:  ./R-dataprocessor/submodules/Dataprocessor_Submodules_Table_Description.xlsx[table_description]
 -- SCRIPTNAME:  331_cre_table_dataproc_submodules_log.sql
 -- TEMPLATE:  template_cre_table.sql
@@ -104,13 +104,13 @@ BEGIN
         IF NOT EXISTS ( -- column not exists (ward_name)
             SELECT 1 FROM information_schema.columns WHERE table_schema = 'db_log' AND table_name = 'dp_mrp_calculations' AND column_name = 'ward_name'
         ) THEN
-            ALTER TABLE db_log.dp_mrp_calculations ADD ward_name varchar;   -- optional – Name of the ward where the patient was during the medication analysis (varchar)
+            ALTER TABLE db_log.dp_mrp_calculations ADD ward_name varchar;   -- Name of the ward where the patient was during the medication analysis or the very first relevant ward (varchar)
         END IF; -- column (ward_name)
 
         IF NOT EXISTS ( -- column not exists (study_phase)
             SELECT 1 FROM information_schema.columns WHERE table_schema = 'db_log' AND table_name = 'dp_mrp_calculations' AND column_name = 'study_phase'
         ) THEN
-            ALTER TABLE db_log.dp_mrp_calculations ADD study_phase varchar;   -- optional – Study phase („PhaseA“, „PhaseBTest“ or „PhaseB“); must be filled if meda_id is not empty (varchar)
+            ALTER TABLE db_log.dp_mrp_calculations ADD study_phase varchar;   -- Study phase („PhaseA“, „PhaseBTest“ or „PhaseB“); must be filled if there was any contact with a observed ward in his medical case (varchar)
         END IF; -- column (study_phase)
 
         IF NOT EXISTS ( -- column not exists (ret_id)
@@ -166,8 +166,8 @@ BEGIN
 	         COALESCE(db.to_char_immutable(enc_id), '#NULL#') || '|||' || -- hash from: FHIR ID of the associated institution contact (enc_id)
           COALESCE(db.to_char_immutable(mrp_calculation_type), '#NULL#') || '|||' || -- hash from: Type of MRP (name of the submodule which has calculated the MRP e.g. “Drug_Disease”, “Drug_Drug”, “Drug_DrugGoup”, “Drug_Kidney”) (mrp_calculation_type)
           COALESCE(db.to_char_immutable(meda_id), '#NULL#') || '|||' || -- hash from: optional - Redcap ID of the medication_analysis_fe (empty if no MedAna exists for this Encounter) (meda_id)
-          COALESCE(db.to_char_immutable(ward_name), '#NULL#') || '|||' || -- hash from: optional – Name of the ward where the patient was during the medication analysis (ward_name)
-          COALESCE(db.to_char_immutable(study_phase), '#NULL#') || '|||' || -- hash from: optional – Study phase („PhaseA“, „PhaseBTest“ or „PhaseB“); must be filled if meda_id is not empty (study_phase)
+          COALESCE(db.to_char_immutable(ward_name), '#NULL#') || '|||' || -- hash from: Name of the ward where the patient was during the medication analysis or the very first relevant ward (ward_name)
+          COALESCE(db.to_char_immutable(study_phase), '#NULL#') || '|||' || -- hash from: Study phase („PhaseA“, „PhaseBTest“ or „PhaseB“); must be filled if there was any contact with a observed ward in his medical case (study_phase)
           COALESCE(db.to_char_immutable(ret_id), '#NULL#') || '|||' || -- hash from: optional – Redcap ID of the generated retrolective_mrpbewertung_fe (ret_id)
           COALESCE(db.to_char_immutable(mrp_proxy_type), '#NULL#') || '|||' || -- hash from: optional – ICD, ATC, OPS, LOINC (mrp_proxy_type)
           COALESCE(db.to_char_immutable(mrp_proxy_code), '#NULL#') || '|||' || -- hash from: optional – Code of the proxy (mrp_proxy_code)
@@ -188,8 +188,8 @@ BEGIN
 	         COALESCE(db.to_char_immutable(enc_id), '#NULL#') || '|||' || -- hash from: FHIR ID of the associated institution contact (enc_id)
           COALESCE(db.to_char_immutable(mrp_calculation_type), '#NULL#') || '|||' || -- hash from: Type of MRP (name of the submodule which has calculated the MRP e.g. “Drug_Disease”, “Drug_Drug”, “Drug_DrugGoup”, “Drug_Kidney”) (mrp_calculation_type)
           COALESCE(db.to_char_immutable(meda_id), '#NULL#') || '|||' || -- hash from: optional - Redcap ID of the medication_analysis_fe (empty if no MedAna exists for this Encounter) (meda_id)
-          COALESCE(db.to_char_immutable(ward_name), '#NULL#') || '|||' || -- hash from: optional – Name of the ward where the patient was during the medication analysis (ward_name)
-          COALESCE(db.to_char_immutable(study_phase), '#NULL#') || '|||' || -- hash from: optional – Study phase („PhaseA“, „PhaseBTest“ or „PhaseB“); must be filled if meda_id is not empty (study_phase)
+          COALESCE(db.to_char_immutable(ward_name), '#NULL#') || '|||' || -- hash from: Name of the ward where the patient was during the medication analysis or the very first relevant ward (ward_name)
+          COALESCE(db.to_char_immutable(study_phase), '#NULL#') || '|||' || -- hash from: Study phase („PhaseA“, „PhaseBTest“ or „PhaseB“); must be filled if there was any contact with a observed ward in his medical case (study_phase)
           COALESCE(db.to_char_immutable(ret_id), '#NULL#') || '|||' || -- hash from: optional – Redcap ID of the generated retrolective_mrpbewertung_fe (ret_id)
           COALESCE(db.to_char_immutable(mrp_proxy_type), '#NULL#') || '|||' || -- hash from: optional – ICD, ATC, OPS, LOINC (mrp_proxy_type)
           COALESCE(db.to_char_immutable(mrp_proxy_code), '#NULL#') || '|||' || -- hash from: optional – Code of the proxy (mrp_proxy_code)
@@ -227,8 +227,8 @@ COMMENT ON COLUMN db_log.dp_mrp_calculations.dp_mrp_calculations_id IS 'Primary 
 COMMENT ON COLUMN db_log.dp_mrp_calculations.enc_id IS 'FHIR ID of the associated institution contact (varchar)';
 COMMENT ON COLUMN db_log.dp_mrp_calculations.mrp_calculation_type IS 'Type of MRP (name of the submodule which has calculated the MRP e.g. “Drug_Disease”, “Drug_Drug”, “Drug_DrugGoup”, “Drug_Kidney”) (varchar)';
 COMMENT ON COLUMN db_log.dp_mrp_calculations.meda_id IS 'optional - Redcap ID of the medication_analysis_fe (empty if no MedAna exists for this Encounter) (varchar)';
-COMMENT ON COLUMN db_log.dp_mrp_calculations.ward_name IS 'optional – Name of the ward where the patient was during the medication analysis (varchar)';
-COMMENT ON COLUMN db_log.dp_mrp_calculations.study_phase IS 'optional – Study phase („PhaseA“, „PhaseBTest“ or „PhaseB“); must be filled if meda_id is not empty (varchar)';
+COMMENT ON COLUMN db_log.dp_mrp_calculations.ward_name IS 'Name of the ward where the patient was during the medication analysis or the very first relevant ward (varchar)';
+COMMENT ON COLUMN db_log.dp_mrp_calculations.study_phase IS 'Study phase („PhaseA“, „PhaseBTest“ or „PhaseB“); must be filled if there was any contact with a observed ward in his medical case (varchar)';
 COMMENT ON COLUMN db_log.dp_mrp_calculations.ret_id IS 'optional – Redcap ID of the generated retrolective_mrpbewertung_fe (varchar)';
 COMMENT ON COLUMN db_log.dp_mrp_calculations.mrp_proxy_type IS 'optional – ICD, ATC, OPS, LOINC (varchar)';
 COMMENT ON COLUMN db_log.dp_mrp_calculations.mrp_proxy_code IS 'optional – Code of the proxy (varchar)';
