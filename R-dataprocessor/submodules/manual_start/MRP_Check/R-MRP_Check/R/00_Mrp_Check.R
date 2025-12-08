3262d70e7bd8716b0e7095d3d5ec2694559110fa
Alexander Strübing <a.leipzig@gmx.de>
2025-12-08 14:38:40 +0100

    Create single merged local and global table as MRP Check result

--- R-dataprocessor/submodules/manual_start/MRP_Check/R-MRP_Check/R/00_Mrp_Check.R
+++ R-dataprocessor/submodules/manual_start/MRP_Check/R-MRP_Check/R/00_Mrp_Check.R
@@ -16,11 +16,36 @@ mrpCheck <- function() {
       ),
       command_arguments = command_arguments
     )
-    mrp_table_lists_all <- calculateMRPs(command_arguments$start_date, command_arguments$end_date)
+    mrp_table_lists_all <- calculateMRPs(command_arguments$start_date, command_arguments$end_date, return_used_resources = "record_ids")
   })
 
-  etlutils::runLevel2("Save calculated MRPs as Excel files", {
-    etlutils::writeExcelFileLocal(mrp_table_lists_all, "MRP_Check_Result")
+  etlutils::runLevel2("Create local MRP result table", {
+    names(mrp_table_lists_all$dp_mrp_calculations)
+    mrp_table_lists_all$retrolektive_mrpbewertung_fe[, c("retrolektive_mrpbewertung_complete",
+                                                         "redcap_repeat_instrument",
+                                                         "redcap_repeat_instance") := NULL]
+    mrp_table_lists_all$dp_mrp_calculations[, c("ret_redcap_repeat_instance",
+                                                "input_file_processed_content_hash",
+                                                "ward_name") := NULL]
+    result <- mrp_table_lists_all$dp_mrp_calculations[mrp_table_lists_all$retrolektive_mrpbewertung_fe, on = "ret_id"]
+    result[, record_id := as.character(record_id)]
+    result <- mrp_table_lists_all$record_ids[result, on = "record_id"]
+  })
+
+  etlutils::runLevel2("Save calculated MRPs as local Excel files", {
+    etlutils::writeExcelFileLocal(result, "MRP_Check_Result_local")
+  })
+
+  etlutils::runLevel2("Create global MRP result table", {
+    id_cols <- grep("_id$", names(result), value = TRUE)
+    for (col in id_cols) {
+      # Create unique integer IDs per column while preserving consistency
+      result[, (col) := .GRP, by = col]
+    }
+  })
+
+  etlutils::runLevel2("Save calculated MRPs as local Excel files", {
+    etlutils::writeExcelFileGlobal(result, "MRP_Check_Result_global")
   })
 
 }
