02d42878701249185d5693d1b4352c220e10fc7f
tatjana.beppler@uni-jena.de <tatjana.beppler@uni-jena.de>
2026-01-27 11:19:09 +0100

    Set correct unit in difftime for age calculation
    (cherry picked from commit 2bf46d51574e160bb093042d31a23d64de18deaf)

--- R-dataprocessor/submodules/manual_start/Statistical_Reports/R-Statistical_Reports/R/02_mergers_and_calculations.R
+++ R-dataprocessor/submodules/manual_start/Statistical_Reports/R-Statistical_Reports/R/02_mergers_and_calculations.R
@@ -377,7 +377,8 @@ calculateAge <- function(merged_table_with_MainEncPeriodStart,
   merged_table_with_age <- merged_table_with_MainEncPeriodStart |>
     dplyr::mutate(age_at_hospitalization = floor(as.numeric(difftime(
       as.Date({{ main_enc_period_start }}),
-      {{ pat_birthdate }}
+      as.Date({{ pat_birthdate }}),
+      units = "days"
     )) / 365.25)) |>
     dplyr::relocate(age_at_hospitalization, .after = {{ pat_birthdate }}) |>
     dplyr::select(-{{ pat_birthdate }}) |>
