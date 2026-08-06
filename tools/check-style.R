355e13fc55813a048b40c4d27e3251a35408111b
fschmidt <florian.schmidt@imise.uni-leipzig.de>
2026-08-06 14:46:18 +0200

    Reduce redundant GitHub Actions checks refs #1339

--- tools/check-style.R
+++ tools/check-style.R
@@ -1,4 +1,18 @@
-source("tools/style.R")
+style_mode <- if (length(commandArgs(trailingOnly = TRUE))) {
+  commandArgs(trailingOnly = TRUE)[[1L]]
+} else {
+  "full"
+}
+
+if (!style_mode %in% c("full", "changed")) {
+  stop("Usage: Rscript tools/check-style.R [full|changed]", call. = FALSE)
+}
+
+if (identical(style_mode, "changed")) {
+  source("tools/style-changed.R")
+} else {
+  source("tools/style.R")
+}
 
 diff_result <- system2("git", c("diff", "--exit-code", "--", existing_style_paths))
 if (!identical(diff_result, 0L)) {
