6de7585e5e57bd04e4ce3a8a5ae16db606561270
tatjana.beppler@uni-jena.de <tatjana.beppler@uni-jena.de>
2026-09-09 09:24:55 +0200

    Fix ward-specific boundary enc filtering #1422

--- R-dataprocessor/submodules/manual_start/Statistical_Reports/R-Statistical_Reports/R/04_prepare-data.R
+++ R-dataprocessor/submodules/manual_start/Statistical_Reports/R-Statistical_Reports/R/04_prepare-data.R
@@ -361,14 +361,14 @@ prepareFeSummaryData <- function(frontend_table, report_period_start, report_per
     # (as approximation the hospital stay is used and therefore the fall_ent_dat is used to determine if the encounter
     # is on the ward within the reporting period)
     frontend_summary_prep <- frontend_summary_prep |>
-      dplyr::filter(is.na(fall_ent_dat) | fall_ent_dat >= max(as.POSIXct(ward_start), as.POSIXct(report_period_start))) |>
-      dplyr::filter(fall_aufn_dat < min(as.POSIXct(ward_end), as.POSIXct(report_period_end))) |>
+      dplyr::filter(is.na(fall_ent_dat) | fall_ent_dat >= pmax(as.POSIXct(ward_start), as.POSIXct(report_period_start))) |>
+      dplyr::filter(fall_aufn_dat < pmin(as.POSIXct(ward_end), as.POSIXct(report_period_end))) |>
       dplyr::distinct()
   } else if (report_period_boundary == "ward_stay") {
     # Filter for encounters that start within the reporting period based on enc_period_start and curated_enc_period_end
     frontend_summary_prep <- frontend_summary_prep |>
-      dplyr::filter(enc_period_start >= max(as.POSIXct(ward_start), as.POSIXct(report_period_start))) |>
-      dplyr::filter(enc_period_start < min(as.POSIXct(ward_end), as.POSIXct(report_period_end))) |>
+      dplyr::filter(enc_period_start >= pmax(as.POSIXct(ward_start), as.POSIXct(report_period_start))) |>
+      dplyr::filter(enc_period_start < pmin(as.POSIXct(ward_end), as.POSIXct(report_period_end))) |>
       dplyr::distinct()
   }
 
