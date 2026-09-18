362c36c59c1010a1b2ec5417507902169029c8d0
fschmidt <florian.schmidt@imise.uni-leipzig.de>
2026-09-18 13:09:55 +0200

    Update version

--- Postgres-cds_hub/sql/base/030_db_parameter.sql
+++ Postgres-cds_hub/sql/base/030_db_parameter.sql
@@ -4,11 +4,11 @@ DO $$
 DECLARE
     ---------------------------------------------------------------------------------------
     -- >>>> WICHTIG Standort / Zeitabhängig <<<< -- Hier bitte Variablen setzen
-    release_version varchar := '2.1.0';
+    release_version varchar := '2.2.0';
     -- GitHup release version
-    release_version_nr int := 210;
+    release_version_nr int := 220;
     -- version number - first 3 digits as integer for comparison
-    release_version_date varchar := '2026-09-03';
+    release_version_date varchar := '2026-09-18';
     -- GitHup (likely) release version date
     pause_after_process_execution varchar := '10';
     -- Pause after copy process execution in second [5-30 sec] - ready to connect
